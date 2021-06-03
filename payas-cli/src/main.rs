ebc9d109d8ea2a5861e54532569ae6fd819d216d
Ramnivas Laddad <ramnivas@ramnivas.com>
2021-06-03 09:08:56 -0700

    Defer reference constraint to after creating tables
    Depending on how we order table creation, we may end up referencing table that is yet to be created. So instead of adding foreign key constraints through REFERENCES, we now add ALTER TABLE ADD CONSTAINTS to the same effect.
    
    Fixes #39

--- payas-cli/src/main.rs
+++ payas-cli/src/main.rs
@@ -1,48 +1,15 @@
 use std::env;
 
-use payas_model::sql::{column::PhysicalColumn, PhysicalTable};
 use payas_parser::{builder::system_builder, parser};
+
+mod schema;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let ast_system = parser::parse_file(&args[1]);
     let system = system_builder::build(ast_system);
 
-    let tables = system.tables;
+    let schema_stmt = schema::schema_stmt(system.tables);
 
-    let table_stmts = tables
-        .iter()
-        .map(|table| create_table(table.1))
-        .collect::<Vec<_>>()
-        .join("\n\n");
-
-    println!("{}", table_stmts);
-}
-
-fn create_table(table: &PhysicalTable) -> String {
-    let column_stmts = table
-        .columns
-        .iter()
-        .map(create_column)
-        .collect::<Vec<_>>()
-        .join(",\n\t");
-
-    format!("CREATE TABLE {} (\n\t{}\n);", table.name, column_stmts)
-}
-
-fn create_column(column: &PhysicalColumn) -> String {
-    let pk_str = if column.is_pk { " PRIMARY KEY" } else { "" };
-
-    let references_str = match column.references {
-        Some(ref references) => format!(" REFERENCES {}", references.table_name),
-        None => "".to_string(),
-    };
-
-    format!(
-        "{} {}{}{}",
-        column.column_name,
-        column.typ.db_type(column.is_autoincrement),
-        pk_str,
-        references_str
-    )
+    println!("{}", schema_stmt);
 }
