0daed82d0a5173a993c6c095285ccd48099b9715
Shadaj Laddad <shadaj@users.noreply.github.com>
2021-07-12 21:47:56 -0700

    Add support for comments and fix syntax highlighting with no-arg annotations (#114)
    * Add support for comments and fix syntax highlighting with no-arg annotations
    
    * Add more comments to the test

--- payas-parser/src/parser/converter.rs
+++ payas-parser/src/parser/converter.rs
@@ -33,6 +33,7 @@ pub fn convert_root(node: Node, source: &[u8], codemap: &CodeMap, source_span: S
         AstSystem {
             models: node
                 .children(&mut cursor)
+                .filter(|n| n.kind() == "declaration")
                 .map(|c| convert_declaration(c, source, source_span))
                 .collect(),
         }
@@ -500,18 +501,26 @@ mod tests {
     fn bb_schema() {
         parsing_test(
             r#"
+        // a short comment
         @table("concerts")
         model Concert {
           id: Int @pk @autoincrement
-          title: String
+          title: String // a comment
+          // another comment
           venue: Venue @column("venueid")
+          /*
+          not_a_field: Int
+          */
         }
 
+        /*
+        a multiline comment
+        */
         @table("venues")
         model Venue {
           id: Int @pk @autoincrement
           name: String
-          concerts: [Concert] @column("venueid")
+          concerts: [Concert /* here too! */] @column("venueid")
         }
         "#,
         );
