7712e1dd57ffe4cee0dc632c9788441cfe3e9c12
Ramnivas Laddad <ramnivas@payalabs.com>
2021-07-30 11:59:49 -0700

    WIP: Before using contained rows in a CTE

--- payas-server/src/data/create_data_param_mapper.rs
+++ payas-server/src/data/create_data_param_mapper.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use async_graphql_value::Value;
 
@@ -6,8 +6,8 @@ use crate::sql::column::Column;
 
 use payas_model::{
     model::{
-        column_id::ColumnId, operation::CreateDataParameter, relation::GqlRelation,
-        types::GqlTypeKind, GqlCompositeTypeKind, GqlField, GqlType,
+        column_id::ColumnId, relation::GqlRelation, types::GqlTypeKind, GqlCompositeTypeKind,
+        GqlField, GqlType,
     },
     sql::column::PhysicalColumn,
 };
@@ -19,56 +19,109 @@ pub struct InsertionRow<'a> {
     pub column_values: HashMap<&'a PhysicalColumn, &'a Column<'a>>,
 }
 
-impl<'a> SQLMapper<'a, InsertionRow<'a>> for CreateDataParameter {
+pub struct InsertionInfo<'a> {
+    pub name: String, // A name suitable to be used as the CTE name
+    pub columns: Vec<&'a PhysicalColumn>,
+    pub values: Vec<Vec<&'a Column<'a>>>,
+}
+
+impl<'a> SQLMapper<'a, (Vec<&'a PhysicalColumn>, Vec<Vec<&'a Column<'a>>>)> for GqlType {
     fn map_to_sql(
         &'a self,
         argument: &'a Value,
         operation_context: &'a OperationContext<'a>,
-    ) -> InsertionRow<'a> {
-        let system = &operation_context.query_context.system;
-        let model_type = &system.mutation_types[self.type_id];
+    ) -> (Vec<&'a PhysicalColumn>, Vec<Vec<&'a Column<'a>>>) {
+        match argument {
+            Value::List(elems) => {
+                let unaligned: Vec<_> = elems
+                    .iter()
+                    .map(|elem| map_single(self, elem, operation_context))
+                    .collect();
 
-        model_type.map_to_sql(argument, operation_context)
+                align(unaligned)
+            }
+            _ => {
+                let raw = map_single(self, argument, operation_context).column_values;
+                let raw: (Vec<_>, Vec<_>) = raw.into_iter().unzip();
+                (raw.0, vec![raw.1])
+            }
+        }
     }
 }
 
-impl<'a> SQLMapper<'a, InsertionRow<'a>> for GqlType {
-    fn map_to_sql(
-        &'a self,
-        argument: &'a Value,
-        operation_context: &'a OperationContext<'a>,
-    ) -> InsertionRow<'a> {
-        let argument = match argument {
-            Value::Variable(name) => operation_context.resolve_variable(name.as_str()).unwrap(),
-            _ => argument,
-        };
+fn align<'a>(
+    unaligned: Vec<InsertionRow<'a>>,
+) -> (Vec<&'a PhysicalColumn>, Vec<Vec<&'a Column<'a>>>) {
+    // Here we may have each mapped element with potentially different set of columns.
+    // For example, if the input is {data: [{a: 1, b: 2}, {a: 3, c: 4}]}, we will have the 'a' key in both
+    // but only 'b' or 'c' keys in others. So we need align columns that can be supplied to an insert statement
+    // (a, b, c), [(1, 2, null), (3, null, 4)]
+    let mut all_keys = HashSet::new();
+    for item in unaligned.iter() {
+        all_keys.extend(item.column_values.keys())
+    }
 
-        let row = match &self.kind {
-            GqlTypeKind::Primitive => panic!(),
-            GqlTypeKind::Composite(GqlCompositeTypeKind { fields, .. }) => fields
-                .iter()
-                .flat_map(|field| {
-                    println!("\n---------");
-                    dbg!(&field.name);
-                    dbg!(argument);
-                    dbg!(operation_context.get_argument_field(argument, &field.name));
-                    match operation_context.get_argument_field(argument, &field.name) {
-                        Some(argument) => match field.relation.self_column() {
-                            Some(key_column_id) => {
-                                map_self_column(key_column_id, field, argument, operation_context)
-                            }
-                            None => {
-                                map_contained(field, argument, operation_context);
-                                None
-                            }
-                        },
-                        None => None,
-                    }
-                })
-                .collect(),
-        };
+    let keys_count = all_keys.len();
 
-        InsertionRow { column_values: row }
+    let mut result = Vec::with_capacity(unaligned.len());
+    for item in unaligned.into_iter() {
+        let mut row = Vec::with_capacity(keys_count);
+        for key in &all_keys {
+            let value = item
+                .column_values
+                .get(key)
+                .copied()
+                .unwrap_or(&Column::Null);
+            row.push(value);
+        }
+
+        result.push(row);
+    }
+
+    (all_keys.into_iter().collect(), result)
+}
+
+/// Map a single item from the data parameter
+fn map_single<'a>(
+    typ: &'a GqlType,
+    argument: &'a Value,
+    operation_context: &'a OperationContext<'a>,
+) -> InsertionRow<'a> {
+    let argument = match argument {
+        Value::Variable(name) => operation_context.resolve_variable(name.as_str()).unwrap(),
+        _ => argument,
+    };
+
+    let fields = match &typ.kind {
+        GqlTypeKind::Primitive => panic!(),
+        GqlTypeKind::Composite(GqlCompositeTypeKind { fields, .. }) => fields,
+    };
+
+    let mut self_row = HashMap::new();
+    let mut other_rows = Vec::new();
+
+    fields.iter().for_each(|field| {
+        // Process fields that map to a column in the current table
+        let field_self_column = field.relation.self_column();
+        let field_arg = operation_context.get_argument_field(argument, &field.name);
+
+        match field_arg {
+            Some(field_arg) => match field_self_column {
+                Some(field_self_column) => {
+                    let (col, value) =
+                        map_self_column(field_self_column, field, field_arg, operation_context);
+                    self_row.insert(col, value);
+                }
+                None => other_rows.push(map_contained(field, field_arg, operation_context)),
+            },
+            None => (), // TODO: Report an error if the field is non-optional
+        }
+    });
+
+    println!("{:?}", other_rows);
+
+    InsertionRow {
+        column_values: self_row,
     }
 }
 
@@ -77,7 +130,7 @@ fn map_self_column<'a>(
     field: &'a GqlField,
     argument: &'a Value,
     operation_context: &'a OperationContext<'a>,
-) -> Option<(&'a PhysicalColumn, &'a Column<'a>)> {
+) -> (&'a PhysicalColumn, &'a Column<'a>) {
     let system = &operation_context.query_context.system;
 
     let key_column = key_column_id.get_column(system);
@@ -97,26 +150,17 @@ fn map_self_column<'a>(
         _ => argument,
     };
     let value_column = operation_context.literal_column(argument_value.clone(), key_column);
-    Some((key_column, value_column))
+    (key_column, value_column)
 }
 
 fn map_contained<'a>(
     field: &'a GqlField,
     argument: &'a Value,
     operation_context: &'a OperationContext<'a>,
-) -> Option<()> {
-    println!("map_contained {:?} {:?}", field, argument);
+) -> (Vec<&'a PhysicalColumn>, Vec<Vec<&'a Column<'a>>>) {
     let system = &operation_context.query_context.system;
 
     let field_type = field.typ.base_type(&system.mutation_types);
 
-    println!("{}", argument);
-    println!("{:?}", field_type);
-    let x = field_type.map_to_sql(argument, operation_context);
-
-    println!("{:?}", x);
-
-    // println!("{:?}", field);
-
-    None
+    field_type.map_to_sql(argument, operation_context)
 }
--- payas-server/src/data/mutation_resolver.rs
+++ payas-server/src/data/mutation_resolver.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 use crate::{
     data::{
         query_resolver::QueryOperations,
@@ -188,51 +186,11 @@ fn insertion_columns<'a>(
     arguments: &'a Arguments,
     operation_context: &'a OperationContext<'a>,
 ) -> Option<(Vec<&'a PhysicalColumn>, Vec<Vec<&'a Column<'a>>>)> {
+    let system = &operation_context.query_context.system;
+    let model_type = &system.mutation_types[data_param.type_id];
+
     let argument_value = super::find_arg(arguments, &data_param.name);
-    argument_value.map(|argument_value| match argument_value {
-        Value::List(elems) => {
-            let unaligned: Vec<_> = elems
-                .iter()
-                .map(|elem| data_param.map_to_sql(elem, operation_context))
-                .collect();
-
-            // Here we may have each mapped element with potentially different set of columns.
-            // For example, if the input is {data: [{a: 1, b: 2}, {a: 3, c: 4}]}, we will have the 'a' key in both
-            // but only 'b' or 'c' keys in others. So we need align columns that can be supplied to an insert statement
-            // (a, b, c), [(1, 2, null), (3, null, 4)]
-            let mut all_keys = HashSet::new();
-            for item in unaligned.iter() {
-                all_keys.extend(item.column_values.keys())
-            }
-
-            let keys_count = all_keys.len();
-
-            let mut result = Vec::with_capacity(unaligned.len());
-            for item in unaligned.into_iter() {
-                let mut row = Vec::with_capacity(keys_count);
-                for key in &all_keys {
-                    let value = item
-                        .column_values
-                        .get(key)
-                        .copied()
-                        .unwrap_or(&Column::Null);
-                    row.push(value);
-                }
-
-                result.push(row);
-            }
-
-            (all_keys.into_iter().collect(), result)
-        }
-        _ => {
-            let raw: (Vec<_>, Vec<_>) = data_param
-                .map_to_sql(argument_value, operation_context)
-                .column_values
-                .into_iter()
-                .unzip();
-            (raw.0, vec![raw.1])
-        }
-    })
+    argument_value.map(|argument_value| model_type.map_to_sql(argument_value, operation_context))
 }
 
 fn update_columns<'a>(
