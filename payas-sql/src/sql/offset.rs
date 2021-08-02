9649c31b3469c5fdc8e25fe571f7eb5245ce97ff
Ramnivas Laddad <ramnivas@payalabs.com>
2021-08-02 15:51:10 -0700

    Support limit and offset in select

---
+++ payas-sql/src/sql/offset.rs
@@ -0,0 +1,11 @@
+use super::{Expression, ExpressionContext, ParameterBinding};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Offset(pub i64);
+
+impl Expression for Offset {
+    fn binding(&self, expression_context: &mut ExpressionContext) -> ParameterBinding {
+        let param_index = expression_context.next_param();
+        ParameterBinding::new(format! {"OFFSET ${}", param_index}, vec![&self.0])
+    }
+}
