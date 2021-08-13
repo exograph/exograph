394767a63a3d5f808c95f0e7ec4bfae180345ab9
Ramnivas Laddad <ramnivas@payalabs.com>
2021-08-13 14:29:21 -0700

    Introspection support for nested update (#150)
    Implement introspection for updating one-to-many fields.
    
    For `Concert` - `[ConcertArtist]` - `Artist` and `ConcertArtist` defined as follows:
    ```clay
    @table("concert_artists")
    model ConcertArtist {
      id: Int @pk @autoincrement
      concert: Concert @column("concert_id")
      artist: Artist @column("artist_id")
      rank: Int
      role: String
    }
    ```
    
    We want to allow mutation that can create/update/delete ConcertArtist making mutations such as following possible:
    ```graphql
    mutation {
    	updateConcert(
        	id: 4, 
        	data: {
            title: "C1-updated", 
            concertArtists: {
              delete: {id: 8},
              update: {id: 4, artist: {id: 10}, rank: 5, role: "main"},
              create: {artist: {id: 16}, rank: 3, role: "accompanying"}
            }
          }
      ) {
        id
        title
        concertArtists {
          id
          artist {
            id
            name
          }
          role
          rank
        }
      }
    }
    ```
    
    Also,
    * Refactor mutation builder
    - Introduce the concept of `Builder`
    - Separate builders for each mutation kind

---
+++ payas-parser/src/builder/delete_mutation_builder.rs
@@ -0,0 +1,67 @@
+//! Build mutation input types associatd with deletion (<Type>DeletionInput) and
+//! the create mutations (delete<Type>, and delete<Type>s)
+
+use payas_model::model::mapped_arena::MappedArena;
+use payas_model::model::naming::ToGqlMutationNames;
+use payas_model::model::types::GqlType;
+use payas_model::model::GqlTypeKind;
+
+use crate::builder::query_builder;
+
+use payas_model::model::operation::MutationKind;
+
+use super::mutation_builder::MutationBuilder;
+use super::resolved_builder::{ResolvedCompositeType, ResolvedType};
+use super::system_builder::SystemContextBuilding;
+use super::Builder;
+
+pub struct DeleteMutationBuilder;
+
+impl Builder for DeleteMutationBuilder {
+    fn type_names(
+        &self,
+        _resolved_composite_type: &ResolvedCompositeType,
+        _models: &MappedArena<ResolvedType>,
+    ) -> Vec<String> {
+        // delete mutations don't need any special input type (the type for the PK and the type for filtering suffice)
+        vec![]
+    }
+
+    /// Expand the mutation input types as well as build the mutation
+    fn build_expanded(&self, building: &mut SystemContextBuilding) {
+        // Since there are no special input types for deletion, no expansion is needed
+
+        for (_, model_type) in building.types.iter() {
+            if let GqlTypeKind::Composite { .. } = &model_type.kind {
+                let model_type_id = building.types.get_id(model_type.name.as_str()).unwrap();
+
+                for mutation in self.build_mutations(model_type_id, model_type, building) {
+                    building.mutations.add(&mutation.name.to_owned(), mutation);
+                }
+            }
+        }
+    }
+}
+
+impl MutationBuilder for DeleteMutationBuilder {
+    fn single_mutation_name(model_type: &GqlType) -> String {
+        model_type.pk_delete()
+    }
+
+    fn single_mutation_kind(
+        model_type: &GqlType,
+        building: &SystemContextBuilding,
+    ) -> MutationKind {
+        MutationKind::Delete(query_builder::pk_predicate_param(model_type, building))
+    }
+
+    fn multi_mutation_name(model_type: &GqlType) -> String {
+        model_type.collection_delete()
+    }
+
+    fn multi_mutation_kind(model_type: &GqlType, building: &SystemContextBuilding) -> MutationKind {
+        MutationKind::Delete(query_builder::collection_predicate_param(
+            model_type, building,
+        ))
+    }
+}
--- payas-parser/src/builder/mod.rs
+++ payas-parser/src/builder/mod.rs
@@ -3,11 +3,69 @@
 mod system_builder;
 
 mod context_builder;
+mod create_mutation_builder;
+mod delete_mutation_builder;
 mod mutation_builder;
 mod order_by_type_builder;
 mod predicate_builder;
 mod query_builder;
+mod reference_input_type_builder;
 mod resolved_builder;
 mod type_builder;
+mod update_mutation_builder;
 
 pub use system_builder::build;
+
+use payas_model::model::{mapped_arena::MappedArena, GqlType, GqlTypeKind};
+
+use self::{
+    resolved_builder::{ResolvedCompositeType, ResolvedType},
+    system_builder::SystemContextBuilding,
+};
+
+// TODO: Ensure it works for all builders (this one makes the assumption that it is building only input types)
+// TODO: Abstract out build_expanded (currently loops in it are repeated in each implementation)
+
+/// Trait for all builders to abstract out the implementation of shallow and expanded building
+pub trait Builder {
+    /// Names of types produced by this builder.
+    /// Shallow building use these type names (since not much else is needed)
+    fn type_names(
+        &self,
+        resolved_composite_type: &ResolvedCompositeType,
+        resolved_types: &MappedArena<ResolvedType>,
+    ) -> Vec<String>;
+
+    fn create_shallow_type(
+        &self,
+        resolved_type: &ResolvedType,
+        resolved_types: &MappedArena<ResolvedType>,
+        building: &mut SystemContextBuilding,
+    ) {
+        if let ResolvedType::Composite(c) = resolved_type {
+            for type_name in self.type_names(c, resolved_types).iter() {
+                building.mutation_types.add(
+                    type_name,
+                    GqlType {
+                        name: type_name.to_string(),
+                        plural_name: "".to_string(), // unused
+                        kind: GqlTypeKind::Primitive,
+                        is_input: true,
+                    },
+                );
+            }
+        }
+    }
+
+    fn build_shallow(
+        &self,
+        resolved_types: &MappedArena<ResolvedType>,
+        building: &mut SystemContextBuilding,
+    ) {
+        for (_, model_type) in resolved_types.iter() {
+            self.create_shallow_type(model_type, resolved_types, building);
+        }
+    }
+
+    fn build_expanded(&self, building: &mut SystemContextBuilding);
+}
