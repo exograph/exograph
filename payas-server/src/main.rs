a2dd947ee20761b39f3a1fe3c70d22169aa91343
Ramnivas Laddad <ramnivas@payalabs.com>
2021-10-12 18:06:45 -0700

    Align clay-server command better with 'clay server'
    - Assume no-args to mean supplying index.claypot
    - Accept both .clay and .claypot extensions (and adjust to make it a .claypot file).

--- payas-server/src/main.rs
+++ payas-server/src/main.rs
@@ -1,11 +1,33 @@
-use std::{env, time::SystemTime};
+use std::{env, process::exit, time::SystemTime};
 
 use payas_server::start_prod_mode;
 
 fn main() {
     let system_start_time = SystemTime::now();
 
-    let model_file = env::args().nth(1).expect("Usage: clay-server <model-file>");
+    let mut args = env::args().skip(1);
 
-    start_prod_mode(&model_file, Some(system_start_time)).unwrap();
+    if args.len() == 0 {
+        // $ clay-server
+        start_prod_mode("index.claypot", Some(system_start_time)).unwrap();
+    } else if args.len() == 1 {
+        let file_name = args.next().unwrap();
+
+        let claypot_file = if file_name.ends_with(".claypot") {
+            // $ clay-server concerts.claypot
+            file_name
+        } else if file_name.ends_with(".clay") {
+            // $ clay-server concerts.clay
+            format!("{}pot", file_name)
+        } else {
+            println!("The input file {} doesn't appear to be a claypot. You need build one with the 'clay build <model-file-name>' command.", file_name);
+            exit(1);
+        };
+
+        start_prod_mode(&claypot_file, Some(system_start_time)).unwrap()
+    } else {
+        // $ clay-server <model-file-name> extra-arguments...
+        println!("Usage: clay-server <claypot-file>");
+        exit(1);
+    }
 }
