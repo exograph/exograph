ab2e605779705b284cb518d998a4705d07d4ff0b
Ramnivas Laddad <ramnivas@payalabs.com>
2021-11-02 17:04:06 -0700

    Watch other resources and not just the model file

---
+++ payas-server/src/model_watcher.rs
@@ -0,0 +1,32 @@
+use anyhow::Result;
+use globwalk;
+use std::path::Path;
+
+use super::watcher;
+
+use std::time::Duration;
+
+pub fn with_watch<T, STARTF, STOPF>(
+    model_path: impl AsRef<Path>,
+    watch_delay: Duration,
+    start: STARTF,
+    stop: STOPF,
+) -> Result<()>
+where
+    STARTF: Fn(bool) -> Result<T>,
+    STOPF: FnMut(&mut T),
+{
+    let model_path = model_path.as_ref().to_path_buf();
+
+    // Watch everything in the parent of the model file, expept for the artifacts we generate (the bundle files)
+    // under the assumption that users won't include files whose names match the *.bundle.* pattern
+    let watched_paths: Vec<_> = globwalk::GlobWalkerBuilder::from_patterns(
+        model_path.parent().unwrap(),
+        &["*", "!*.bundle.*"],
+    )
+    .build()?
+    .filter_map(|file| file.ok().map(|f| f.path().to_owned()))
+    .collect();
+
+    watcher::with_watch(watched_paths, watch_delay, start, stop)
+}
