2ef209c4d954aad5ee486bf31f98d077d050f37a
Ramnivas Laddad <ramnivas@payalabs.com>
2021-11-03 10:12:20 -0700

    Add a clarifying comment

--- payas-server/src/model_watcher.rs
+++ payas-server/src/model_watcher.rs
@@ -19,7 +19,9 @@ where
     let model_path = model_path.as_ref().to_path_buf();
 
     // Watch everything in the parent of the model file, expept for the artifacts we generate (the bundle files)
-    // under the assumption that users won't include files whose names match the *.bundle.* pattern
+    // under the assumption that users won't include files whose names match the *.bundle.* pattern.
+    // An alternative would be to parse the model file and look for the artifacts used, but that will
+    // require parsing the js/ts files referred to in the model files recursively to gather all the dependencies.
     let watched_paths: Vec<_> = globwalk::GlobWalkerBuilder::from_patterns(
         model_path.parent().unwrap(),
         &["*", "!*.bundle.*"],
