c0a60581b5c96646f4ac730adde6abc36625e497
___ <tekul@users.noreply.github.com>
2021-12-22 19:50:21 +0000

    Add a rayon thread pool to the test runner in payas-test (#296)
    This limits the number of threads that are used when running integration
    test files. A server is spawned for every test file, so it is currently
    possible to run out of database connections if all the tests are being
    run at once - postgres has a default maximum of 100 connections. Even
    without this limitation it's probably more efficient to limit the number
    of servers spawned, depending on the resources of the test machine.
    
    The number of threads used is the minimum of the number of tests or
    double the number of CPUs on the machine.
    
    Also added reporting of time taken and number of CPUs when running the
    tests.

--- payas-test/src/lib.rs
+++ payas-test/src/lib.rs
@@ -1,10 +1,14 @@
 mod claytest;
 
 use anyhow::{bail, Result};
-use claytest::loader::load_testfiles_from_dir;
+use claytest::loader::{load_testfiles_from_dir, ParsedTestfile};
 use claytest::runner::run_testfile;
+use rayon::ThreadPoolBuilder;
+use std::cmp::min;
 use std::path::Path;
+use std::sync::mpsc;
 
+/// Loads test files from the supplied directory and runs them using a thread pool.
 pub fn run(directory: &Path) -> Result<()> {
     println!(
         "{} {} {}",
@@ -14,33 +18,42 @@ pub fn run(directory: &Path) -> Result<()> {
         directory.to_str().unwrap(),
         ansi_term::Color::Blue.bold().paint("..."),
     );
+    let start_time = std::time::Instant::now();
+    let cpus = num_cpus::get();
 
-    // Load testfiles
+    let database_url = std::env::var("CLAY_TEST_DATABASE_URL").expect("CLAY_TEST_DATABASE_URL");
+
     let testfiles = load_testfiles_from_dir(Path::new(&directory)).unwrap();
-    let number_of_tests = testfiles.len() * 2; // *2 because we run each testfile twice: dev mode and qproduction mode
+    let number_of_tests = testfiles.len() * 2; // *2 because we run each testfile twice: dev mode and production mode
 
-    // Run testfiles in parallel
-    let mut test_results: Vec<_> = testfiles
-        .into_iter()
-        .flat_map(|t| {
-            let t_dev = t.clone();
-            vec![
-                std::thread::spawn(move || {
-                    run_testfile(
-                        &t_dev,
-                        std::env::var("CLAY_TEST_DATABASE_URL").unwrap(),
-                        true, // dev_mode
-                    )
-                }),
-                std::thread::spawn(move || {
-                    run_testfile(&t, std::env::var("CLAY_TEST_DATABASE_URL").unwrap(), false)
-                }),
-            ]
-        })
-        .collect::<Vec<_>>()
-        .into_iter()
-        .map(|j| j.join().unwrap())
-        .collect();
+    // Estimate an optimal pool size
+    let pool_size = min(number_of_tests, cpus * 2);
+    let pool = ThreadPoolBuilder::new()
+        .num_threads(pool_size)
+        .build()
+        .unwrap();
+
+    let (tx, rx) = mpsc::channel();
+
+    let run_test_in_pool = |file: ParsedTestfile, is_dev_mode: bool| {
+        let tx = tx.clone();
+        let url = database_url.clone();
+
+        pool.spawn(move || {
+            let result = run_testfile(&file, url, is_dev_mode);
+            tx.send(result).unwrap();
+        });
+    };
+
+    // Run testfiles in parallel using the thread pool in both production and dev modes
+    for file in testfiles.iter() {
+        run_test_in_pool(file.clone(), false);
+        run_test_in_pool(file.clone(), true);
+    }
+
+    drop(tx);
+
+    let mut test_results: Vec<_> = rx.into_iter().collect();
 
     test_results.sort_by(|a, b| {
         if a.is_ok() && b.is_err() {
@@ -57,6 +70,7 @@ pub fn run(directory: &Path) -> Result<()> {
             std::cmp::Ordering::Equal
         }
     });
+
     test_results.reverse();
 
     let mut number_of_succeeded_tests = 0;
@@ -84,13 +98,15 @@ pub fn run(directory: &Path) -> Result<()> {
     };
 
     println!(
-        "{} {} {} out of {} total",
+        "{} {} {} out of {} total in {} seconds ({} cpus)",
         ansi_term::Color::Blue.bold().paint("* Test results:"),
         status,
         ansi_term::Style::new()
             .bold()
             .paint(format!("{} passed", number_of_succeeded_tests)),
-        number_of_tests
+        number_of_tests,
+        start_time.elapsed().as_secs(),
+        cpus,
     );
 
     if success {
