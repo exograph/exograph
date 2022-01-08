3f58b0dd2458890f63914d0e0c0f13ab7d8e5234
Luke Taylor <tekul@pm.me>
2022-01-08 20:16:10 +0000

    Make DB stuff async (switch to deadpool and tokio-postgres)
    Comment out payas-sql test code as it's complicated to make async
    just now because it uses Drop to delete databases.

--- payas-cli/src/commands/import.rs
+++ payas-cli/src/commands/import.rs
@@ -3,7 +3,7 @@
 use anyhow::Result;
 use payas_model::spec::ToModel;
 use payas_model::sql::database::Database;
-use payas_sql::spec::SchemaSpec;
+use payas_sql::spec::{SchemaSpec, WithIssues};
 use std::{fs::File, io::Write, path::PathBuf, time::SystemTime};
 
 use super::command::Command;
@@ -15,13 +15,14 @@ pub struct ImportCommand {
 
 impl Command for ImportCommand {
     fn run(&self, _system_start_time: Option<SystemTime>) -> Result<()> {
-        let database = Database::from_env(Some(1))?; // TODO: error handling here
-        let mut client = database.get_client()?;
+        // Create runtime and make the rest of this an async block
+        // (then block on it)
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .build()
+            .unwrap();
 
         let mut issues = Vec::new();
-
-        let mut schema = SchemaSpec::from_db(&mut client)?;
-
+        let mut schema = rt.block_on(import_schema())?;
         let mut model = schema.value.to_model();
 
         issues.append(&mut schema.issues);
@@ -35,4 +36,11 @@ impl Command for ImportCommand {
         println!("\nClaytip model written to `{}`", self.output.display());
         Ok(())
     }
+}
+
+async fn import_schema() -> Result<WithIssues<SchemaSpec>> {
+    let database = Database::from_env(Some(1))?; // TODO: error handling here
+    let client = database.get_client().await?;
+    let schema = SchemaSpec::from_db(&client).await?;
+    Ok(schema)
 }
--- payas-sql/src/sql/database.rs
+++ payas-sql/src/sql/database.rs
@@ -1,12 +1,10 @@
 use anyhow::{bail, Context, Result};
-use once_cell::sync::OnceCell;
-use std::{env, ops::DerefMut};
+use std::env;
 
+use deadpool_postgres::{Client, Manager, ManagerConfig, Pool, RecyclingMethod};
 use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
-use postgres::{Client, Config};
 use postgres_openssl::MakeTlsConnector;
-use r2d2::Pool;
-use r2d2_postgres::PostgresConnectionManager;
+use tokio_postgres::Config;
 
 const URL_PARAM: &str = "CLAY_DATABASE_URL";
 const USER_PARAM: &str = "CLAY_DATABASE_USER";
@@ -15,21 +13,19 @@ const CONNECTION_POOL_SIZE_PARAM: &str = "CLAY_CONNECTION_POOL_SIZE";
 const CHECK_CONNECTION_ON_STARTUP: &str = "CLAY_CHECK_CONNECTION_ON_STARTUP";
 
 pub struct Database {
-    config: Config,
-    pool_size: u32,
-    pool: OnceCell<Pool<PostgresConnectionManager<MakeTlsConnector>>>,
+    pool: Pool,
 }
 
 impl<'a> Database {
     // pool_size_override useful when we want to explicitly control the pool size (for example, to 1, when importing database schema)
-    pub fn from_env(pool_size_override: Option<u32>) -> Result<Self> {
+    pub fn from_env(pool_size_override: Option<usize>) -> Result<Self> {
         let url = env::var(URL_PARAM).context("CLAY_DATABASE_URL must be provided")?;
         let user = env::var(USER_PARAM).ok();
         let password = env::var(PASSWORD_PARAM).ok();
         let pool_size = pool_size_override.unwrap_or_else(|| {
             env::var(CONNECTION_POOL_SIZE_PARAM)
                 .ok()
-                .map(|pool_str| pool_str.parse::<u32>().unwrap())
+                .map(|pool_str| pool_str.parse::<usize>().unwrap())
                 .unwrap_or(10)
         });
         let check_connection = env::var(CHECK_CONNECTION_ON_STARTUP)
@@ -41,8 +37,8 @@ impl<'a> Database {
     }
 
     pub fn from_env_helper(
-        pool_size: u32,
-        check_connection: bool,
+        pool_size: usize,
+        _check_connection: bool,
         url: String,
         user: Option<String>,
         password: Option<String>,
@@ -67,35 +63,30 @@ impl<'a> Database {
             bail!("Database user must be specified through as a part of CLAY_DATABASE_URL or through CLAY_DATABASE_USER")
         }
 
-        let pool = OnceCell::new();
+        let mut builder = SslConnector::builder(SslMethod::tls())?;
+        builder.set_verify(SslVerifyMode::NONE);
+        let connector = MakeTlsConnector::new(builder.build());
 
-        let db = Self {
+        let manager = Manager::from_config(
             config,
-            pool_size,
-            pool,
-        };
+            connector, // or tokio_postgres::NoTls,
+            ManagerConfig {
+                recycling_method: RecyclingMethod::Fast,
+            },
+        );
 
-        if check_connection {
-            db.get_pool()?;
-        }
+        let pool = Pool::builder(manager).max_size(pool_size).build().unwrap();
+
+        let db = Self { pool };
+
+        // if check_connection {
+        //     let _ = db.get_client().await?;
+        // }
 
         Ok(db)
     }
 
-    pub fn get_client(&self) -> Result<impl DerefMut<Target = Client>> {
-        Ok(self.get_pool()?.get()?)
-    }
-
-    fn get_pool(&self) -> Result<&Pool<PostgresConnectionManager<MakeTlsConnector>>> {
-        self.pool.get_or_try_init::<_, anyhow::Error>(|| {
-            let mut builder = SslConnector::builder(SslMethod::tls())?;
-            builder.set_verify(SslVerifyMode::NONE);
-            let connector = MakeTlsConnector::new(builder.build());
-
-            let manager = PostgresConnectionManager::new(self.config.clone(), connector);
-            let pool_builder = Pool::builder().max_size(self.pool_size);
-
-            Ok(pool_builder.build(manager)?)
-        })
+    pub async fn get_client(&self) -> Result<Client> {
+        Ok(self.pool.get().await?)
     }
 }
