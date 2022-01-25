53062f1dff826323e34c4716f3d33191f6b91a05
Ramnivas Laddad <ramnivas@payalabs.com>
2022-01-25 13:58:24 -0800

    WIP: debug CI failure

--- payas-parser/build.rs
+++ payas-parser/build.rs
@@ -1,8 +1,16 @@
 use std::fs;
 use std::path::PathBuf;
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 fn main() {
+    Command::new("tree-sitter")
+        .arg("--version")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to execute 'tree-sitter --version'")
+        .wait_with_output()
+        .expect("Fail to wait for 'tree-sitter --version'");
+
     let sitter_out = Command::new("tree-sitter")
         .arg("generate")
         .current_dir(fs::canonicalize("./grammar").unwrap())
