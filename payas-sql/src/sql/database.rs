2cefdd37ea531ffdfd7c69a95a8a6b281f97eb1e
Ramnivas Laddad <ramnivas@payalabs.com>
2022-01-26 15:52:42 -0800

    Refactor the SSL config creation code to a separate method

--- payas-sql/src/sql/database.rs
+++ payas-sql/src/sql/database.rs
@@ -31,6 +31,70 @@ impl<'a> Database {
                 .unwrap_or(10)
         });
 
+        let ssl_config = Self::create_ssl_config()?;
+
+        let check_connection = env::var(CHECK_CONNECTION_ON_STARTUP)
+            .ok()
+            .map(|pool_str| pool_str.parse::<bool>().unwrap())
+            .unwrap_or(true);
+
+        Self::from_env_helper(pool_size, check_connection, url, user, password, ssl_config)
+    }
+
+    fn from_env_helper(
+        pool_size: usize,
+        _check_connection: bool,
+        url: String,
+        user: Option<String>,
+        password: Option<String>,
+        ssl_config: Option<(SslMethod, SslVerifyMode)>,
+    ) -> Result<Self> {
+        use std::str::FromStr;
+
+        let mut config =
+            Config::from_str(&url).context("Failed to parse PostgreSQL connection string")?;
+
+        if let Some(user) = &user {
+            config.user(user);
+        }
+        if let Some(password) = &password {
+            config.password(password);
+        }
+
+        if config.get_user() == None {
+            bail!("Database user must be specified through as a part of CLAY_DATABASE_URL or through CLAY_DATABASE_USER")
+        }
+
+        let manager_config = ManagerConfig {
+            recycling_method: RecyclingMethod::Fast,
+        };
+
+        let manager = match ssl_config {
+            Some((ssl_method, ssl_verify_mode)) => {
+                let mut builder = SslConnector::builder(ssl_method)?;
+                builder.set_verify(ssl_verify_mode);
+                let connector = MakeTlsConnector::new(builder.build());
+                Manager::from_config(config, connector, manager_config)
+            }
+            None => Manager::from_config(config, tokio_postgres::NoTls, manager_config),
+        };
+
+        let pool = Pool::builder(manager).max_size(pool_size).build().unwrap();
+
+        let db = Self { pool };
+
+        // if check_connection {
+        //     let _ = db.get_client().await?;
+        // }
+
+        Ok(db)
+    }
+
+    pub async fn get_client(&self) -> Result<Client> {
+        Ok(self.pool.get().await?)
+    }
+
+    fn create_ssl_config() -> Result<Option<(SslMethod, SslVerifyMode)>> {
         let ssl_method = env::var(SSL_METHOD_PARAM)
             .ok()
             .map(
@@ -54,7 +118,7 @@ impl<'a> Database {
             })
             .unwrap_or_else(|| Ok(None))?;
 
-        let ssl_info = match (ssl_method, ssl_verify_mode) {
+        let ssl_config = match (ssl_method, ssl_verify_mode) {
             (Some(ssl_method), Some(ssl_verify_mode)) => Some((ssl_method, ssl_verify_mode)),
             (Some(ssl_method), None) => {
                 eprintln!("SSL verify mode not provided through the CLAY_SSL_VERIFY_MODE env. Using default SSL verify mode: 'peer'");
@@ -67,64 +131,6 @@ impl<'a> Database {
             _ => None,
         };
 
-        let check_connection = env::var(CHECK_CONNECTION_ON_STARTUP)
-            .ok()
-            .map(|pool_str| pool_str.parse::<bool>().unwrap())
-            .unwrap_or(true);
-
-        Self::from_env_helper(pool_size, check_connection, url, user, password, ssl_info)
-    }
-
-    fn from_env_helper(
-        pool_size: usize,
-        _check_connection: bool,
-        url: String,
-        user: Option<String>,
-        password: Option<String>,
-        ssl_info: Option<(SslMethod, SslVerifyMode)>,
-    ) -> Result<Self> {
-        use std::str::FromStr;
-
-        let mut config =
-            Config::from_str(&url).context("Failed to parse PostgreSQL connection string")?;
-
-        if let Some(user) = &user {
-            config.user(user);
-        }
-        if let Some(password) = &password {
-            config.password(password);
-        }
-
-        if config.get_user() == None {
-            bail!("Database user must be specified through as a part of CLAY_DATABASE_URL or through CLAY_DATABASE_USER")
-        }
-
-        let manager_config = ManagerConfig {
-            recycling_method: RecyclingMethod::Fast,
-        };
-
-        let manager = match ssl_info {
-            Some((ssl_method, ssl_verify_mode)) => {
-                let mut builder = SslConnector::builder(ssl_method)?;
-                builder.set_verify(ssl_verify_mode);
-                let connector = MakeTlsConnector::new(builder.build());
-                Manager::from_config(config, connector, manager_config)
-            }
-            None => Manager::from_config(config, tokio_postgres::NoTls, manager_config),
-        };
-
-        let pool = Pool::builder(manager).max_size(pool_size).build().unwrap();
-
-        let db = Self { pool };
-
-        // if check_connection {
-        //     let _ = db.get_client().await?;
-        // }
-
-        Ok(db)
-    }
-
-    pub async fn get_client(&self) -> Result<Client> {
-        Ok(self.pool.get().await?)
+        Ok(ssl_config)
     }
 }
