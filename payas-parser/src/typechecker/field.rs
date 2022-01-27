90b6386dcdb3bd5f70590ad16bb3d9d58670ac4b
Andy Chun <andy.chun@payalabs.com>
2022-01-26 16:57:01 -0800

    check that default value functions are appropriate for their type

--- payas-parser/src/typechecker/field.rs
+++ payas-parser/src/typechecker/field.rs
@@ -1,9 +1,11 @@
 use std::collections::HashMap;
 
-use codemap_diagnostic::Diagnostic;
+use codemap_diagnostic::{Diagnostic, Level, SpanLabel, SpanStyle};
 use payas_model::model::mapped_arena::MappedArena;
 
-use crate::ast::ast_types::{AstField, AstFieldDefault, AstFieldType, Untyped};
+use crate::ast::ast_types::{
+    AstField, AstFieldDefault, AstFieldDefaultKind, AstFieldType, Untyped,
+};
 
 use super::annotation::{AnnotationSpec, AnnotationTarget};
 use super::{AnnotationMap, Scope, Type, TypecheckFrom, Typed};
@@ -44,6 +46,56 @@ impl TypecheckFrom<AstField<Untyped>> for AstField<Typed> {
             .map(|default_value| default_value.pass(type_env, annotation_env, scope, errors))
             .unwrap_or(false);
 
-        typ_changed || annot_changed || default_value_changed
+        let default_value_types_changed = if let Some(AstFieldDefault {
+            kind: AstFieldDefaultKind::Function(fn_name, _),
+            ..
+        }) = &self.default_value
+        {
+            match fn_name.as_str() {
+                "now" => match self.typ.name().as_str() {
+                    "Instant" | "LocalDate" | "LocalTime" | "LocalDateTime" => false,
+
+                    _ => {
+                        errors.push(Diagnostic {
+                            level: Level::Error,
+                            message: format!("now() can only be used for time-related types",),
+                            code: Some("C000".to_string()),
+                            spans: vec![SpanLabel {
+                                span: self.span,
+                                style: SpanStyle::Primary,
+                                label: Some("field containing now()".to_string()),
+                            }],
+                        });
+
+                        true
+                    }
+                },
+
+                "autoincrement" => match self.typ.name().as_str() {
+                    "Int" => false,
+
+                    _ => {
+                        errors.push(Diagnostic {
+                            level: Level::Error,
+                            message: format!("autoincrement() can only be used for Ints",),
+                            code: Some("C000".to_string()),
+                            spans: vec![SpanLabel {
+                                span: self.span,
+                                style: SpanStyle::Primary,
+                                label: Some("field containing autoincrement()".to_string()),
+                            }],
+                        });
+
+                        true
+                    }
+                },
+
+                _ => false,
+            }
+        } else {
+            false
+        };
+
+        typ_changed || annot_changed || default_value_changed || default_value_types_changed
     }
 }
