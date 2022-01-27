d7979dbc7d05d69dc1db3879fab51feb1ca7ce26
Ramnivas Laddad <ramnivas@payalabs.com>
2022-01-27 12:46:38 -0800

    Offer choices for database SSL configuration (#347)
    Earlier, we always set SSL method to `tls` and SSL verify to `None`,
    but this is not enough for some databases configurations (and an 
    overkill for others such as a local database).
    
    We now support two env variables to control these two aspects of SSL
    configuration: CLAY_SSL_METHOD env to control the ssl method (with
    possible values of `tls` and `dtls` as well as CLAY_SSL_NO_VERIFY to
    turn off the SSL verification.

--- payas-sql/src/sql/database.rs
+++ payas-sql/src/sql/database.rs
@@ -1,4 +1,4 @@
-use anyhow::{bail, Context, Result};
+use anyhow::{anyhow, bail, Context, Result};
 use std::env;
 
 use deadpool_postgres::{Client, Manager, ManagerConfig, Pool, RecyclingMethod};
@@ -11,6 +11,8 @@ const USER_PARAM: &str = "CLAY_DATABASE_USER";
 const PASSWORD_PARAM: &str = "CLAY_DATABASE_PASSWORD";
 const CONNECTION_POOL_SIZE_PARAM: &str = "CLAY_CONNECTION_POOL_SIZE";
 const CHECK_CONNECTION_ON_STARTUP: &str = "CLAY_CHECK_CONNECTION_ON_STARTUP";
+const SSL_METHOD_PARAM: &str = "CLAY_SSL_METHOD"; // Possible values: "tls" and "dtls"
+const SSL_NO_VERIFY_PARAM: &str = "CLAY_SSL_NO_VERIFY"; // boolean (default: false)
 
 pub struct Database {
     pool: Pool,
@@ -28,21 +30,24 @@ impl<'a> Database {
                 .map(|pool_str| pool_str.parse::<usize>().unwrap())
                 .unwrap_or(10)
         });
+
+        let ssl_config = Self::create_ssl_config()?;
+
         let check_connection = env::var(CHECK_CONNECTION_ON_STARTUP)
             .ok()
             .map(|pool_str| pool_str.parse::<bool>().unwrap())
             .unwrap_or(true);
 
-        Self::from_env_helper(pool_size, check_connection, url, user, password, None)
+        Self::from_env_helper(pool_size, check_connection, url, user, password, ssl_config)
     }
 
-    pub fn from_env_helper(
+    fn from_env_helper(
         pool_size: usize,
         _check_connection: bool,
         url: String,
         user: Option<String>,
         password: Option<String>,
-        db_name_override: Option<String>,
+        ssl_config: Option<(SslMethod, SslVerifyMode)>,
     ) -> Result<Self> {
         use std::str::FromStr;
 
@@ -55,25 +60,24 @@ impl<'a> Database {
         if let Some(password) = &password {
             config.password(password);
         }
-        if let Some(db_name) = &db_name_override {
-            config.dbname(db_name);
-        }
 
         if config.get_user() == None {
             bail!("Database user must be specified through as a part of CLAY_DATABASE_URL or through CLAY_DATABASE_USER")
         }
 
-        let mut builder = SslConnector::builder(SslMethod::tls())?;
-        builder.set_verify(SslVerifyMode::NONE);
-        let connector = MakeTlsConnector::new(builder.build());
+        let manager_config = ManagerConfig {
+            recycling_method: RecyclingMethod::Fast,
+        };
 
-        let manager = Manager::from_config(
-            config,
-            connector, // or tokio_postgres::NoTls,
-            ManagerConfig {
-                recycling_method: RecyclingMethod::Fast,
-            },
-        );
+        let manager = match ssl_config {
+            Some((ssl_method, ssl_verify_mode)) => {
+                let mut builder = SslConnector::builder(ssl_method)?;
+                builder.set_verify(ssl_verify_mode);
+                let connector = MakeTlsConnector::new(builder.build());
+                Manager::from_config(config, connector, manager_config)
+            }
+            None => Manager::from_config(config, tokio_postgres::NoTls, manager_config),
+        };
 
         let pool = Pool::builder(manager).max_size(pool_size).build().unwrap();
 
@@ -88,5 +92,50 @@ impl<'a> Database {
 
     pub async fn get_client(&self) -> Result<Client> {
         Ok(self.pool.get().await?)
+    }
+
+    fn create_ssl_config() -> Result<Option<(SslMethod, SslVerifyMode)>> {
+        let ssl_method = env::var(SSL_METHOD_PARAM)
+            .ok()
+            .map(
+                |env_str| match env_str.as_str().to_ascii_lowercase().as_str() {
+                    "tls" => Ok(Some(SslMethod::tls())),
+                    "dtls" => Ok(Some(SslMethod::dtls())),
+                    _ => Err(anyhow!(
+                        "Invalid SSL method: {}. Env {} must be set to either 'tls' or 'dtls'",
+                        env_str,
+                        SSL_METHOD_PARAM
+                    )),
+                },
+            )
+            .unwrap_or_else(|| Ok(None))?;
+
+        let ssl_no_verify = env::var(SSL_NO_VERIFY_PARAM)
+            .ok()
+            .map(|env_str| match env_str.parse::<bool>() {
+                Ok(b) => Ok(Some(b)),
+                Err(_) => Err(anyhow!(
+                    "Invalid {} value: {}. It must be set to 'true' or 'false'",
+                    SSL_NO_VERIFY_PARAM,
+                    env_str,
+                )),
+            })
+            .unwrap_or_else(|| Ok(None))?;
+
+        if ssl_method.is_none() && ssl_no_verify == Some(false) {
+            bail!(
+                "{} must be set to 'tls' or 'dtls' when {} is set to 'false'",
+                SSL_METHOD_PARAM,
+                SSL_NO_VERIFY_PARAM
+            )
+        }
+
+        let ssl_config = match (ssl_method, ssl_no_verify) {
+            (Some(ssl_method), Some(false)) => Some((ssl_method, SslVerifyMode::PEER)),
+            (Some(ssl_method), Some(true)) => Some((ssl_method, SslVerifyMode::NONE)),
+            _ => None,
+        };
+
+        Ok(ssl_config)
     }
 }
