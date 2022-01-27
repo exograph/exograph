4ff87e288bc527bd17429461b97ea772aa3877ca
Andy Chun <andy.chun@payalabs.com>
2022-01-27 14:27:46 -0800

    Check between literal types and field type in default value field

--- payas-parser/src/typechecker/field.rs
+++ payas-parser/src/typechecker/field.rs
@@ -4,7 +4,7 @@ use codemap_diagnostic::{Diagnostic, Level, SpanLabel, SpanStyle};
 use payas_model::model::mapped_arena::MappedArena;
 
 use crate::ast::ast_types::{
-    AstField, AstFieldDefault, AstFieldDefaultKind, AstFieldType, Untyped,
+    AstExpr, AstField, AstFieldDefault, AstFieldDefaultKind, AstFieldType, Untyped,
 };
 
 use super::annotation::{AnnotationSpec, AnnotationTarget};
@@ -46,24 +46,23 @@ impl TypecheckFrom<AstField<Untyped>> for AstField<Typed> {
             .map(|default_value| default_value.pass(type_env, annotation_env, scope, errors))
             .unwrap_or(false);
 
-        let default_value_types_changed = if let Some(AstFieldDefault {
-            kind: AstFieldDefaultKind::Function(fn_name, _),
-            ..
-        }) = &self.default_value
-        {
-            match fn_name.as_str() {
+        let default_value_types_changed = match &self.default_value {
+            Some(AstFieldDefault {
+                kind: AstFieldDefaultKind::Function(fn_name, _),
+                ..
+            }) => match fn_name.as_str() {
                 "now" => match self.typ.name().as_str() {
                     "Instant" | "LocalDate" | "LocalTime" | "LocalDateTime" => false,
 
                     _ => {
                         errors.push(Diagnostic {
                             level: Level::Error,
-                            message: format!("now() can only be used for time-related types",),
+                            message: "now() can only be used for time-related types".to_string(),
                             code: Some("C000".to_string()),
                             spans: vec![SpanLabel {
                                 span: self.span,
                                 style: SpanStyle::Primary,
-                                label: Some("field containing now()".to_string()),
+                                label: None,
                             }],
                         });
 
@@ -77,12 +76,12 @@ impl TypecheckFrom<AstField<Untyped>> for AstField<Typed> {
                     _ => {
                         errors.push(Diagnostic {
                             level: Level::Error,
-                            message: format!("autoincrement() can only be used for Ints",),
+                            message: "autoincrement() can only be used on Ints".to_string(),
                             code: Some("C000".to_string()),
                             spans: vec![SpanLabel {
                                 span: self.span,
                                 style: SpanStyle::Primary,
-                                label: Some("field containing autoincrement()".to_string()),
+                                label: None,
                             }],
                         });
 
@@ -91,9 +90,59 @@ impl TypecheckFrom<AstField<Untyped>> for AstField<Typed> {
                 },
 
                 _ => false,
+            },
+
+            Some(AstFieldDefault {
+                kind: AstFieldDefaultKind::Value(expr),
+                ..
+            }) => {
+                let type_name = self.typ.name();
+                let mut assert_type = |types_allowed: &[&str]| {
+                    if !types_allowed.contains(&type_name.as_str()) {
+                        let types_allowed: String = types_allowed.join(", ");
+
+                        errors.push(Diagnostic {
+                            level: Level::Error,
+                            message:
+                                "Literal specified for default value is not a valid type for field."
+                                    .to_string(),
+                            code: Some("C000".to_string()),
+                            spans: vec![SpanLabel {
+                                span: *expr.span(),
+                                style: SpanStyle::Primary,
+                                label: Some(format!("should be of type {}", types_allowed)),
+                            }],
+                        });
+
+                        true
+                    } else {
+                        false
+                    }
+                };
+
+                match *expr {
+                    AstExpr::StringLiteral(_, _) => assert_type(&["String"]),
+                    AstExpr::BooleanLiteral(_, _) => assert_type(&["Boolean"]),
+                    AstExpr::NumberLiteral(_, _) => assert_type(&["Int", "Float"]),
+
+                    _ => {
+                        errors.push(Diagnostic {
+                            level: Level::Error,
+                            message: "Non-literal specified in default value field.".to_string(),
+                            code: Some("C000".to_string()),
+                            spans: vec![SpanLabel {
+                                span: *expr.span(),
+                                style: SpanStyle::Primary,
+                                label: Some("should be string, boolean, or a number".to_string()),
+                            }],
+                        });
+
+                        true
+                    }
+                }
             }
-        } else {
-            false
+
+            _ => false,
         };
 
         typ_changed || annot_changed || default_value_changed || default_value_types_changed
