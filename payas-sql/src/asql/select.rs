bcf71b7436c0b4750a80f5483440f99e40dfd8db
Ramnivas Laddad <ramnivas@payalabs.com>
2022-02-07 18:39:11 -0800

    WIP: Need to deal with nested selects

--- payas-sql/src/asql/select.rs
+++ payas-sql/src/asql/select.rs
@@ -1,16 +1,17 @@
 use crate::{
     asql::{
         column_path::{ColumnPath, ColumnPathLink},
+        selection::SelectionSQL,
         util,
     },
-    sql::{column::Column, predicate::Predicate, Limit, Offset, PhysicalTable, Select},
+    sql::{predicate::Predicate, Limit, Offset, PhysicalTable, Select},
 };
 
 use super::{order_by::AbstractOrderBy, predicate::AbstractPredicate, selection::Selection};
 
 pub struct AbstractSelect<'a> {
     pub table: &'a PhysicalTable,
-    pub selection: Vec<Selection<'a>>,
+    pub selection: Selection<'a>,
     pub predicate: Option<AbstractPredicate<'a>>,
     pub order_by: Option<AbstractOrderBy<'a>>,
     pub offset: Option<Offset>,
@@ -50,14 +51,10 @@ impl<'a> AbstractSelect<'a> {
 
         let join = util::compute_join(self.table, columns_paths);
 
-        let columns = self
-            .selection
-            .iter()
-            .map(|s| match s {
-                Selection::Physical(pc) => Column::Physical(pc).into(),
-                Selection::Compound(_) => todo!(),
-            })
-            .collect();
+        let columns = match self.selection.to_sql() {
+            SelectionSQL::Single(elem) => vec![elem.into()],
+            SelectionSQL::Seq(elems) => elems.into_iter().map(|elem| elem.into()).collect(),
+        };
 
         Select {
             underlying: join,
@@ -81,7 +78,7 @@ mod tests {
         asql::{
             column_path::{ColumnPath, ColumnPathLink},
             predicate::AbstractPredicate,
-            selection::Selection,
+            selection::{Selection, SelectionElement},
             test_util::TestSetup,
         },
         sql::ExpressionContext,
@@ -100,7 +97,7 @@ mod tests {
              }| {
                 let aselect = AbstractSelect {
                     table: concerts_table,
-                    selection: vec![Selection::Physical(concerts_id_column)],
+                    selection: Selection::Seq(vec![SelectionElement::Physical(concerts_id_column)]),
                     predicate: None,
                     order_by: None,
                     offset: None,
@@ -132,7 +129,7 @@ mod tests {
                 let predicate = AbstractPredicate::Eq(concert_id_path, literal);
                 let aselect = AbstractSelect {
                     table: concerts_table,
-                    selection: vec![Selection::Physical(concerts_id_column)],
+                    selection: Selection::Seq(vec![SelectionElement::Physical(concerts_id_column)]),
                     predicate: Some(predicate),
                     order_by: None,
                     offset: None,
@@ -147,6 +144,79 @@ mod tests {
                     r#"select "concerts"."id" from "concerts" WHERE "concerts"."id" = $1"#,
                     5
                 );
+            },
+        );
+    }
+
+    #[test]
+    fn non_nested_json() {
+        TestSetup::with_setup(
+            |TestSetup {
+                 concerts_table,
+                 concerts_id_column,
+                 ..
+             }| {
+                let aselect = AbstractSelect {
+                    table: concerts_table,
+                    selection: Selection::Json(
+                        vec![("id", SelectionElement::Physical(concerts_id_column))],
+                        true,
+                    ),
+                    predicate: None,
+                    order_by: None,
+                    offset: None,
+                    limit: None,
+                };
+
+                let select = aselect.to_sql();
+                let mut expr = ExpressionContext::default();
+                let binding = select.binding(&mut expr);
+                assert_binding!(
+                    binding,
+                    r#"select coalesce(json_agg(json_build_object('id', "concerts"."id")), '[]'::json)::text from "concerts""#
+                );
+            },
+        );
+    }
+
+    #[test]
+    fn nested_json() {
+        TestSetup::with_setup(
+            |TestSetup {
+                 concerts_table,
+                 concerts_id_column,
+                 venues_id_column,
+                 ..
+             }| {
+                let aselect = AbstractSelect {
+                    table: concerts_table,
+                    selection: Selection::Json(
+                        vec![
+                            ("id", SelectionElement::Physical(concerts_id_column)),
+                            (
+                                "venue",
+                                SelectionElement::Compound(Selection::Json(
+                                    vec![("id", SelectionElement::Physical(venues_id_column))],
+                                    false,
+                                )),
+                            ),
+                        ],
+                        true,
+                    ),
+                    predicate: None,
+                    order_by: None,
+                    offset: None,
+                    limit: None,
+                };
+
+                let select = aselect.to_sql();
+                let mut expr = ExpressionContext::default();
+                let binding = select.binding(&mut expr);
+                assert_binding!(
+                    binding,
+                    r#"select coalesce(json_agg(json_build_object('id', "concerts"."id", 'venue', (select json_build_object('id', "venues"."id") from "venues" WHERE "concerts"."venueid" = "venues"."id"))), '[]'::json)::text from "concerts""#
+                );
+                //select coalesce(json_agg(json_build_object('id', "concerts"."id", 'venue', json_build_object('id', "venues"."id"))), '[]'::json)::text from "concerts"
             },
         );
     }
--- payas-sql/src/asql/selection.rs
+++ payas-sql/src/asql/selection.rs
@@ -1,8 +1,57 @@
-use crate::sql::column::PhysicalColumn;
-
-use super::select::AbstractSelect;
+use crate::sql::column::{Column, PhysicalColumn};
 
 pub enum Selection<'a> {
+    Seq(Vec<SelectionElement<'a>>),
+    Json(Vec<(&'a str, SelectionElement<'a>)>, bool),
+}
+
+pub enum SelectionSQL<'a> {
+    Single(Column<'a>),
+    Seq(Vec<Column<'a>>),
+}
+
+impl<'a> Selection<'a> {
+    pub fn to_sql(&self) -> SelectionSQL<'a> {
+        match self {
+            Selection::Seq(seq) => SelectionSQL::Seq(
+                seq.iter()
+                    .map(|s| match s {
+                        SelectionElement::Physical(pc) => Column::Physical(pc),
+                        SelectionElement::Compound(_) => todo!(),
+                    })
+                    .collect(),
+            ),
+            Selection::Json(seq, multi) => {
+                let object_elems = seq
+                    .iter()
+                    .map(|(k, s)| (k.to_string(), s.to_sql().into()))
+                    .collect();
+
+                let json_obj = Column::JsonObject(object_elems);
+
+                if *multi {
+                    SelectionSQL::Single(Column::JsonAgg(Box::new(json_obj.into())))
+                } else {
+                    SelectionSQL::Single(json_obj)
+                }
+            }
+        }
+    }
+}
+
+pub enum SelectionElement<'a> {
     Physical(&'a PhysicalColumn),
-    Compound(AbstractSelect<'a>),
+    Compound(Selection<'a>),
+}
+
+impl<'a> SelectionElement<'a> {
+    pub fn to_sql(&self) -> Column<'a> {
+        match self {
+            SelectionElement::Physical(pc) => Column::Physical(pc),
+            SelectionElement::Compound(s) => match s.to_sql() {
+                SelectionSQL::Single(elem) => elem,
+                SelectionSQL::Seq(_) => todo!(),
+            },
+        }
+    }
 }
