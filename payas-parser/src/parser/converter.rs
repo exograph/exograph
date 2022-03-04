4247f2a77e1d9a7d2e9fae2a45f9a26f9bf8acc0
Ramnivas Laddad <ramnivas@payalabs.com>
2022-03-04 13:28:27 -0800

    Upgrade to Rust 1.59.0

--- payas-parser/src/parser/converter.rs
+++ payas-parser/src/parser/converter.rs
@@ -30,15 +30,11 @@ pub fn convert_root(
     Ok(AstSystem {
         models: node
             .children(&mut cursor)
-            .filter(|n| n.kind() == "declaration")
-            .map(|c| convert_declaration_to_model(c, source, source_span))
-            .flatten()
+            .filter_map(|c| convert_declaration_to_model(c, source, source_span))
             .collect::<Vec<_>>(),
         services: node
             .children(&mut cursor)
-            .filter(|n| n.kind() == "declaration")
-            .map(|c| convert_declaration_to_service(c, source, source_span, filepath))
-            .flatten()
+            .filter_map(|c| convert_declaration_to_service(c, source, source_span, filepath))
             .collect::<Vec<_>>(),
     })
 }
