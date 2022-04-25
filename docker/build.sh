3cb9db880811e0e8e773040049b084db99fbc2fb
Ramnivas Laddad <ramnivas@payalabs.com>
2022-04-24 19:50:36 -0700

    Make Fly.io integration work with the new docker setup

--- docker/build.sh
+++ docker/build.sh
@@ -17,4 +17,6 @@ else
     exit 1
 fi
 
+docker build -t clay-builder -f docker/Dockerfile --build-arg BUILD_DIR="$BUILD_DIR" --build-arg BUILD_FLAG="$BUILD_FLAG" --target clay-builder .
+
 docker build -t clay -f docker/Dockerfile --build-arg BUILD_DIR="$BUILD_DIR" --build-arg BUILD_FLAG="$BUILD_FLAG" .
