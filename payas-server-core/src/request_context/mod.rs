0010bf845cbe225f97e511d1c7815849ae8bcaab
Andy Chun <andy.chun@payalabs.com>
2022-05-19 17:43:53 -0700

    cargo clippy

--- payas-server-core/src/request_context/mod.rs
+++ payas-server-core/src/request_context/mod.rs
@@ -124,7 +124,7 @@ impl<'a> RequestContext<'a> {
     pub async fn extract_context(&self, context: &ContextType) -> Result<Value> {
         self.test_values
             .get(&context.name)
-            .ok_or(anyhow!(
+            .ok_or_else(|| anyhow!(
                 "Context type {} does not exist in test values",
                 &context.name
             ))
