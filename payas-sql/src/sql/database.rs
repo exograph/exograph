16ba8c0b5f08baddb128a28663724302a949f6a2
Ramnivas Laddad <ramnivas@payalabs.com>
2022-06-11 17:35:49 -0700

    Support commonly found postgres URLs
    This issue was encountered when trying to integrate with YugabyteDB, which issues the URL in the
    form of:
    postgresql://<user>:<password>@<host>:<port>/<db>?ssl=true&sslmode=verify-full&sslrootcert=<crt-file-path>
    
    The tokio-postgres library does not support these ssl params. While we could have asked users to
    simplify this URL and supply additional params such as `sslrootcert` as an env var, it is something
    we have to explain and could be confusing.
    
    This PR cleans up the URL and configures the SSL connectors according to those parameters.

--- payas-sql/src/sql/database.rs
+++ payas-sql/src/sql/database.rs
@@ -4,7 +4,7 @@ use std::env;
 use deadpool_postgres::{Client, Manager, ManagerConfig, Pool, RecyclingMethod};
 use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
 use postgres_openssl::MakeTlsConnector;
-use tokio_postgres::Config;
+use tokio_postgres::{config::SslMode, Config};
 
 const URL_PARAM: &str = "CLAY_DATABASE_URL";
 const USER_PARAM: &str = "CLAY_DATABASE_USER";
@@ -44,15 +44,44 @@ impl<'a> Database {
     fn from_env_helper(
         pool_size: usize,
         _check_connection: bool,
-        url: String,
+        url_string: String,
         user: Option<String>,
         password: Option<String>,
         ssl_config: Option<(SslMethod, SslVerifyMode)>,
     ) -> Result<Self> {
         use std::str::FromStr;
 
-        let mut config =
-            Config::from_str(&url).context("Failed to parse PostgreSQL connection string")?;
+        let url = url::Url::parse(&url_string).context("Invalid URL")?;
+
+        let mut ssl_param: Option<String> = None;
+        let mut ssl_mode: Option<String> = None;
+        let mut ssl_root_cert = None;
+
+        // Remove parameters from the url that typical postgres URL includes (for example, with YugabyteDB),
+        // but the tokio-rust-postgres driver doesn't support yet.
+        // Instead capture those parameters and use them later in the connection/ssl config.
+        let query = url.query_pairs().filter(|(name, value)| {
+            if name == "ssl" {
+                ssl_param = Some(value.to_string());
+                false
+            } else if name == "sslmode" {
+                ssl_mode = Some(value.to_string());
+                false
+            } else if name == "sslrootcert" {
+                ssl_root_cert = Some(value.to_string());
+                false
+            } else {
+                true
+            }
+        });
+
+        let mut cleaned_url = url.clone();
+        cleaned_url.query_pairs_mut().clear().extend_pairs(query);
+
+        // We need to replace '+' (encoded from a space character) with '%20' since the tokio-rust-postgres driver doesn't seem to support
+        // the encoding that uses '+' for a space.
+        let mut config = Config::from_str(cleaned_url.as_str().replace('+', "%20").as_str())
+            .context("Failed to parse PostgreSQL connection string")?;
 
         if let Some(user) = &user {
             config.user(user);
@@ -65,6 +94,29 @@ impl<'a> Database {
             bail!("Database user must be specified through as a part of CLAY_DATABASE_URL or through CLAY_DATABASE_USER")
         }
 
+        // See: https://jdbc.postgresql.org/documentation/head/ssl-client.html
+        // 1. "ssl" parameter is a quick way to specify SSL mode. If it is true, then it has the same effect as setting "sslmode" to "verify-full".
+        //    So we process this first.
+        if let Some(ssl_param) = ssl_param {
+            let ssl_mode = match ssl_param.as_str() {
+                "true" => SslMode::Require,
+                "false" => SslMode::Disable,
+                _ => bail!("Invalid 'ssl' parameter value {ssl_param}"),
+            };
+            config.ssl_mode(ssl_mode);
+        }
+        // 2. The tokio-postgres library doesn't have a way to map all possible values of "sslmode", so we pick the nearest stricter mode.
+        //    We process this the next to allow any refinement of the SSL mode set through the simpler "ssl" parameter.
+        if let Some(ssl_mode) = ssl_mode {
+            let ssl_mode = match ssl_mode.as_str() {
+                "verify-full" | "verify-ca" | "require" => SslMode::Require,
+                "prefer" | "allow" => SslMode::Prefer,
+                "disable" => SslMode::Disable,
+                _ => bail!("Invalid 'sslmode' parameter value {ssl_mode}"),
+            };
+            config.ssl_mode(ssl_mode);
+        }
+
         let manager_config = ManagerConfig {
             recycling_method: RecyclingMethod::Fast,
         };
@@ -73,6 +125,9 @@ impl<'a> Database {
             Some((ssl_method, ssl_verify_mode)) => {
                 let mut builder = SslConnector::builder(ssl_method)?;
                 builder.set_verify(ssl_verify_mode);
+                if let Some(ssl_root_cert) = ssl_root_cert {
+                    builder.set_ca_file(&ssl_root_cert)?;
+                }
                 let connector = MakeTlsConnector::new(builder.build());
                 Manager::from_config(config, connector, manager_config)
             }
