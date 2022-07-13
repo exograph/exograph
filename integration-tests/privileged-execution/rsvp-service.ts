7df1274fc4ed5a491a52824255c0c390880a9160
Ramnivas Laddad <ramnivas@payalabs.com>
2022-07-13 09:31:45 -0700

    Add integration tests

--- integration-tests/privileged-execution/rsvp-service.ts
+++ integration-tests/privileged-execution/rsvp-service.ts
@@ -2,6 +2,18 @@ const existingRsvpQuery = `
   query rsvps($email: String) {
     rsvps(where: {email: {eq: $email}}) {
       id
+      email
+      count
+    }
+  }
+`;
+
+const createRsvpMutation = `
+  mutation ($email: String, $count: Int!) {
+    createRsvp(data: {email: $email, count: $count}) {
+      id
+      email
+      count
     }
   }
 `;
@@ -10,6 +22,8 @@ const updateRsvpMutation = `
   mutation updateRsvp($id: Int!, $rsvp: RsvpUpdateInput!) {
     updateRsvp(id: $id, data: $rsvp) {
       id
+      email
+      count
     }
   }
 `;
@@ -20,26 +34,36 @@ const adminContext = {
   }
 }
 
-export async function processRsvp(operation: Operation, claytip: ClaytipPriv) {
-  const query = operation.query();
-  const email = query.arguments.data.email;
+interface RsvpConfirmation {
+  email: string
+  count: number
+}
+
+// Perform an upsert operation. In real world app, this would also send a confirmation email etc.
+export async function processRsvp(email: string, count: number, claytip: ClaytipPriv): Promise<RsvpConfirmation> {
   const existing = await claytip.executeQueryPriv(existingRsvpQuery, {
     email,
   }, adminContext);
 
   if (existing.rsvps.length > 0) {
     const existingRsvp = existing.rsvps[0];
-    const count = query.arguments.data.count;
 
-    return (await claytip.executeQueryPriv(updateRsvpMutation, {
-      id: existingRsvp.id,
-      rsvp: {
-        email,
-        count,
-      }
-    }, adminContext)).updateRsvp;
-
+    if (existingRsvp.count !== count) {
+      return (await claytip.executeQueryPriv(updateRsvpMutation, {
+        id: existingRsvp.id,
+        rsvp: {
+          email,
+          count,
+        }
+      }, adminContext)).updateRsvp;
+    } else {
+      return existingRsvp;
+    }
   };
+  console.log(`No existing RSVP for ${email}`);
 
-  return await operation.proceed();
+  return (await claytip.executeQueryPriv(createRsvpMutation, {
+    email,
+    count
+  }, adminContext)).createRsvp;
 }
