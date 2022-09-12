a224bdca461404ac9390bdb8246fbd0960a26422
Andy Chun <andy.chun@payalabs.com>
2022-09-12 14:47:04 -0700

    Invoke verification logic before starting server in `clay serve` (#505)
    This PR invokes the schema verification logic against the database model before starting the server during `clay serve`. When verification fails, the command will prompt the user for directions.

--- payas-cli/src/commands/serve.rs
+++ payas-cli/src/commands/serve.rs
@@ -1,7 +1,14 @@
-use anyhow::Result;
-use std::{path::PathBuf, time::SystemTime};
+use anyhow::{anyhow, Result};
+use std::{
+    io::{stdin, stdout, Write},
+    path::PathBuf,
+    time::SystemTime,
+};
 
-use crate::util::watcher;
+use crate::{
+    commands::schema::verify::{verify, VerificationErrors},
+    util::watcher,
+};
 
 use super::command::Command;
 
@@ -13,6 +20,44 @@ pub struct ServeCommand {
 
 impl Command for ServeCommand {
     fn run(&self, _system_start_time: Option<SystemTime>) -> Result<()> {
-        watcher::start_watcher(&self.model, self.port, || Ok(()))
+        watcher::start_watcher(&self.model, self.port, || {
+            println!("Verifying new model...");
+
+            loop {
+                let verification_result = verify(&self.model, None);
+
+                match verification_result {
+                    Err(e @ VerificationErrors::ModelNotCompatible(_)) => {
+                        println!("The schema of the current database is not compatible with the current model for the following reasons:");
+                        println!("{}", e);
+                        println!("Select an option:");
+                        print!("[c]ontinue without fixing, (p)ause and fix manually: ");
+                        stdout().flush()?;
+
+                        let mut input: String = String::new();
+                        let result = std::io::stdin()
+                            .read_line(&mut input)
+                            .map(|_| input.trim())?;
+
+                        match result {
+                            "p" => {
+                                println!("Paused. Press enter to re-verify.");
+
+                                let mut line = String::new();
+                                stdin().read_line(&mut line)?;
+                            }
+                            _ => {
+                                println!("Continuing...");
+                                break Ok(());
+                            }
+                        }
+                    }
+                    _ => {
+                        break verification_result
+                            .map_err(|e| anyhow!("Error during verification: {}", e))
+                    }
+                }
+            }
+        })
     }
 }
