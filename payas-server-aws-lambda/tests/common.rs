8e9efe2266c8cacbd3d119e06d73fedb940bb0ae
Andy Chun <andy.chun@payalabs.com>
2022-09-22 11:09:31 -0700

    Set envvars for Lambda unit test earlier

--- payas-server-aws-lambda/tests/common.rs
+++ payas-server-aws-lambda/tests/common.rs
@@ -8,15 +8,15 @@ pub async fn test_query(json_input: Value, clay_model: &str, expected: Value) {
     let context = lambda_runtime::Context::default();
     let event = lambda_runtime::LambdaEvent::new(json_input, context);
 
+    // HACK: some envvars need to be set to create a SystemContext
+    {
+        std::env::set_var("CLAY_CHECK_CONNECTION_ON_STARTUP", "false");
+        std::env::set_var("CLAY_DATABASE_URL", "postgres://a@dummy-value");
+    }
+
     let model_system =
         payas_parser::build_system_from_str(clay_model, "index.clay".to_string()).unwrap();
     let system_context = Arc::new(create_system_context_with_model_system(model_system).unwrap());
-
-    // HACK: the envvars CLAY_INTROSPECTION and CLAY_DATABASE_URL need to be set to resolve anything
-    {
-        std::env::set_var("CLAY_INTROSPECTION", "true");
-        std::env::set_var("CLAY_DATABASE_URL", "dummy-value");
-    }
 
     let result = resolve(event, system_context).await.unwrap();
 
