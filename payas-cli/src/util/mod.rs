102dbe03d32a9e3ada0bb482aa421ba3cb55d1d8
Andy Chun <andy.chun@payalabs.com>
2022-09-22 15:05:57 -0700

    Consolidate common arguments between CLI subcommands (#509)
    Common arguments taken by CLI subcommands (such as model path and database URL) have been merged into one. Common routines for opening databases and output buffers have also been merged.

--- payas-cli/src/util/mod.rs
+++ payas-cli/src/util/mod.rs
@@ -1,28 +1,39 @@
 use anyhow::{anyhow, Result};
+use payas_sql::{database_error::DatabaseError, Database};
 use std::{
     fs::File,
-    io::{self, stdin, Read, Write},
+    io::{self, stdin, stdout, Read, Write},
     path::Path,
 };
 
 pub(crate) mod watcher;
 
-pub fn open_file_for_output(output: &Path) -> Result<File> {
-    if output.exists() {
-        print!(
-            "File `{}` already exists. Overwrite? [y/N]: ",
-            output.display()
-        );
-        io::stdout().flush()?;
+pub fn open_file_for_output(output: Option<&Path>) -> Result<Box<dyn Write>> {
+    if let Some(output) = output {
+        if output.exists() {
+            print!(
+                "File `{}` already exists. Overwrite? [y/N]: ",
+                output.display()
+            );
+            io::stdout().flush()?;
 
-        let char = stdin().bytes().next().unwrap().unwrap();
+            let char = stdin().bytes().next().unwrap().unwrap();
 
-        if char == b'y' {
-            Ok(File::create(output)?)
-        } else {
-            Err(anyhow!("Not overwriting file"))
+            if char != b'y' {
+                return Err(anyhow!("Not overwriting file"));
+            }
         }
+
+        Ok(Box::new(File::create(output)?))
     } else {
-        Ok(File::create(output)?)
+        Ok(Box::new(stdout()))
+    }
+}
+
+pub fn open_database(database: Option<&str>) -> Result<Database, DatabaseError> {
+    if let Some(database) = database {
+        Ok(Database::from_db_url(database)?)
+    } else {
+        Ok(Database::from_env(Some(1))?)
     }
 }
