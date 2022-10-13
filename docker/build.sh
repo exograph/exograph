b2f0e8c93bd06c2e6073dfd8839631eee046e8bd
Ramnivas Laddad <ramnivas@payalabs.com>
2022-10-13 12:54:00 -0700

    Update Dockerfile to match the latest layout (#537)
    Since we have a lot more packages and we no longer have a single
    prefix, we shift the generation of the Dockerfile to a script.
    We also cleanup up the artifacts from running the empty projects
    a bit more thoroughly.
    
    Fixes #536

--- docker/build.sh
+++ docker/build.sh
@@ -11,8 +11,8 @@ buildType="$2" # "release" or "debug"
 # TODO: Resolve the openssl issues and then "BASE_IMAGE=debian:buster-slim"
 
 ## DEFAULTS ##
-BUILD_IMAGE=rust:1.63.0-buster # image to build Claytip with
-BASE_IMAGE=rust:1.63.0-slim-buster # image to use when actually running Claytip
+BUILD_IMAGE=rust:1.64.0-buster # image to build Claytip with
+BASE_IMAGE=rust:1.64.0-slim-buster # image to use when actually running Claytip
 DEPENDENCY_STYLE=deb # how to install or setup dependencies
 TAG_SUFFIX="" # docker tag suffix
 
@@ -49,6 +49,91 @@ else
     exit 1
 fi
 
+# Tuples of (name kind path)
+declare -a SUBCRATES=(
+    "cli bin crates"
+    "server-actix bin crates"
+    "server-aws-lambda bin crates"
+    "builder lib crates"
+    "resolver lib crates"
+    "testing lib crates"
+    "payas-sql lib libs"
+    "payas-deno lib libs"
+    "payas-wasm lib libs"
+    "core-model lib crates\/core-subsystem"
+    "core-model-builder lib crates\/core-subsystem"
+    "core-plugin lib crates\/core-subsystem"
+    "core-resolver lib crates\/core-subsystem"
+    "database-model lib crates\/database-subsystem"
+    "database-model-builder lib crates\/database-subsystem"
+    "database-resolver lib crates\/database-subsystem"
+    "deno-model lib crates\/deno-subsystem"
+    "deno-model-builder lib crates\/deno-subsystem"
+    "deno-resolver lib crates\/deno-subsystem"
+    "wasm-model lib crates\/wasm-subsystem"
+    "wasm-model-builder lib crates\/wasm-subsystem"
+    "wasm-resolver lib crates\/wasm-subsystem"
+    "introspection-resolver lib crates\/introspection-subsystem"
+    "subsystem-model-builder-util lib crates\/subsystem-util"
+    "subsystem-model-util lib crates\/subsystem-util"
+)
+
+compute_create_empty_projects() {
+    local RESULT
+
+    for crate_info in "${SUBCRATES[@]}"
+    do
+        set -- $crate_info
+        name="$1"
+        kind="$2"
+        path="$3"
+        RESULT="${RESULT}RUN USER=root cargo new --vcs none --$kind $path\/$name\n"
+    done
+
+    echo $RESULT
+}
+
+compute_copy_cargo_tomls() {
+    local RESULT
+
+    for crate_info in "${SUBCRATES[@]}"
+    do
+        set -- $crate_info
+        name="$1"
+        path="$3"
+        RESULT="${RESULT}COPY .\/$path\/$name\/Cargo.toml .\/$path\/$name\/Cargo.toml\n"
+    done
+
+    echo $RESULT
+}
+
+compute_rm_deps() {
+    local RESULT
+
+    artifacts_dir="target\/\${BUILD_DIR}"
+    deps_dir="$artifacts_dir\/deps"
+    for crate_info in "${SUBCRATES[@]}"
+    do
+        set -- $crate_info
+        name="$1"
+        path="$3"
+        module_name=$(echo $name | sed 's/-/_/g')
+
+        # Remove sources that were created by the empty projects
+        RESULT="${RESULT}RUN rm $path\/$name\/src\/\*.rs "
+        # Remove the artifacts created by the empty projects
+        ## First, all the lib* files for our modules
+        RESULT="${RESULT}\&\& rm -f ${artifact_dir}\/lib${module_name}.\* "
+        ## Then, all the .d files for our modules (lib<module_name>.rlib, lib<module_name>.rmeta, <module_name>.d)
+        RESULT="${RESULT}\&\& rm -f ${deps_dir}\/${module_name}* \&\& rm -f ${deps_dir}\/lib${module_name}* "
+        ## Also remove the fingerprint files for our modules (note the names here use the crate name, not the module name)
+        RESULT="${RESULT}\&\& rm -rf ${artifact_dir}\/.fingerprint\/${name}*\n"
+    done
+
+    echo $RESULT
+}
+
+
 # Generates Dockerfile.generated and builds a docker image from it to the specified target.
 # Final image is tagged with the specified tag.
 docker_build() {
@@ -72,6 +157,14 @@ docker_build() {
         | sed -e '/%%BUILD_SETUP%%/ {' -e "r $BUILD_SETUP" -e 'd' -e '}' \
         | sed -e '/%%RUNTIME_SETUP%%/ {' -e "r $RUNTIME_SETUP" -e 'd' -e '}' \
         > $GENERATED_DOCKERFILE
+
+    CREATE_EMPTY_PROJECTS=$(compute_create_empty_projects)
+    COPY_CARGO_TOMLS=$(compute_copy_cargo_tomls)
+    RM_DEPS=$(compute_rm_deps)
+
+    sed -i "s/%%CREATE_EMPTY_PROJECTS%%/$CREATE_EMPTY_PROJECTS/" $GENERATED_DOCKERFILE
+    sed -i "s/%%COPY_CARGO_TOMLS%%/$COPY_CARGO_TOMLS/" $GENERATED_DOCKERFILE
+    sed -i "s/%%RM_DEPS%%/$RM_DEPS/g" $GENERATED_DOCKERFILE
 
     docker build \
             -t $TAG \
