cdc553d6802d0faf30c6c69f737d74cc0461b1f3
Andy Chun <andy.chun@payalabs.com>
2022-11-10 14:19:53 -0800

    Up Docker base image to 1.65.0

--- docker/build.sh
+++ docker/build.sh
@@ -11,8 +11,8 @@ buildType="$2" # "release" or "debug"
 # TODO: Resolve the openssl issues and then "BASE_IMAGE=debian:buster-slim"
 
 ## DEFAULTS ##
-BUILD_IMAGE=rust:1.64.0-buster # image to build Claytip with
-BASE_IMAGE=rust:1.64.0-slim-buster # image to use when actually running Claytip
+BUILD_IMAGE=rust:1.65.0-buster # image to build Claytip with
+BASE_IMAGE=rust:1.65.0-slim-buster # image to use when actually running Claytip
 DEPENDENCY_STYLE=deb # how to install or setup dependencies
 TAG_SUFFIX="" # docker tag suffix
 
