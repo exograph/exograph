89581dc6b5a55434f4ea06f540e30e3649ee4891
Ramnivas Laddad <ramnivas@payalabs.com>
2022-11-29 13:07:23 -0800

    Fix async blocking for the verify cli command (#576)
    Use nested runtime instead of async blocking for the verify command.
    
    Co-authored-by: Andy Chun <andy.chun@payalabs.com>

--- crates/cli/src/commands/schema/verify.rs
+++ crates/cli/src/commands/schema/verify.rs
@@ -24,7 +24,12 @@ pub struct VerifyCommand {
 
 impl Command for VerifyCommand {
     fn run(&self, _system_start_time: Option<SystemTime>) -> Result<()> {
-        let verification_result = verify(&self.model, self.database.as_deref());
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_io()
+            .build()
+            .unwrap();
+
+        let verification_result = rt.block_on(verify(&self.model, self.database.as_deref()));
 
         match &verification_result {
             Ok(()) => eprintln!("This model is compatible with the database schema!"),
@@ -62,33 +67,33 @@ impl Display for VerificationErrors {
     }
 }
 
-pub fn verify(model: &Path, database: Option<&str>) -> Result<(), VerificationErrors> {
-    let rt = tokio::runtime::Builder::new_current_thread()
-        .enable_io()
-        .build()
-        .unwrap();
+pub async fn verify(model: &Path, database: Option<&str>) -> Result<(), VerificationErrors> {
+    let database = open_database(database).map_err(VerificationErrors::PostgresError)?;
+    let client = database
+        .get_client()
+        .await
+        .map_err(VerificationErrors::PostgresError)?;
 
-    rt.block_on(async {
-            let database = open_database(database).map_err(VerificationErrors::PostgresError)?;
-            let client = database.get_client().await.map_err(VerificationErrors::PostgresError)?;
+    // import schema from db
+    let db_schema = SchemaSpec::from_db(&client)
+        .await
+        .map_err(VerificationErrors::PostgresError)?;
+    for issue in &db_schema.issues {
+        println!("{}", issue);
+    }
 
-            // import schema from db
-            let db_schema = SchemaSpec::from_db(&client).await.map_err(VerificationErrors::PostgresError)?;
-            for issue in &db_schema.issues {
-                println!("{}", issue);
-            }
+    // parse provided model
+    let postgres_subsystem =
+        util::create_postgres_system(model).map_err(VerificationErrors::ParserError)?;
+    let model_schema = SchemaSpec::from_model(postgres_subsystem.tables.into_iter().collect());
 
-            // parse provided model
-            let postgres_subsystem = util::create_postgres_system(model).map_err(VerificationErrors::ParserError)?;
-            let model_schema = SchemaSpec::from_model(postgres_subsystem.tables.into_iter().collect());
+    // generate diff
+    let migration = db_schema.value.diff(&model_schema);
 
-            // generate diff
-            let migration = db_schema.value.diff(&model_schema);
+    let mut errors = vec![];
 
-            let mut errors = vec![];
-
-            for op in migration.iter() {
-                match op {
+    for op in migration.iter() {
+        match op {
                     SchemaOp::CreateTable { table } => errors.push(format!("The table `{}` exists in the model, but does not exist in the database.", table.name)),
                     SchemaOp::CreateColumn { column } => errors.push(format!("The column `{}` in the table `{}` exists in the model, but does not exist in the database table.", column.column_name, column.table_name)),
                     SchemaOp::SetColumnDefaultValue { column, default_value } => errors.push(format!("The default value for column `{}` in table `{}` does not match `{}`", column.column_name, column.table_name, default_value)),
@@ -98,12 +103,11 @@ pub fn verify(model: &Path, database: Option<&str>) -> Result<(), VerificationEr
                     SchemaOp::SetNotNull { column } => errors.push(format!("The model requires that the column `{}` in table `{}` is not nullable. All records in the database must have a non-null value for this column before migration.", column.column_name, column.table_name)),
                     _ => {}
                 }
-            }
+    }
 
-            if !errors.is_empty() {
-                Err(VerificationErrors::ModelNotCompatible(errors))
-            } else {
-                Ok(())
-            }
-        })
+    if !errors.is_empty() {
+        Err(VerificationErrors::ModelNotCompatible(errors))
+    } else {
+        Ok(())
+    }
 }
