2cf1c2c2572792acddc7650f0372c1c9de001f95
Andy Chun <andy.chun@payalabs.com>
2022-12-01 11:56:31 -0800

    Re-export codemap-diagnostic and async-graphql-parser under core-plugin-interface

--- crates/core-subsystem/core-plugin-interface/src/lib.rs
+++ crates/core-subsystem/core-plugin-interface/src/lib.rs
@@ -3,6 +3,9 @@ pub mod export_utils;
 
 pub mod interface;
 
+pub use async_graphql_parser;
+pub use codemap_diagnostic;
+
 pub use core_model;
 pub use core_model_builder;
 pub use core_plugin_shared::*;
