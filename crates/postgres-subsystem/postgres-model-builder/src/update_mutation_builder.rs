3942b8ac5141720f86faa0e7a88cd18168ca9c86
Ramnivas Laddad <ramnivas@payalabs.com>
2022-12-30 11:39:06 -0800

    Update treatment of the pk field in nested update

--- crates/postgres-subsystem/postgres-model-builder/src/update_mutation_builder.rs
+++ crates/postgres-subsystem/postgres-model-builder/src/update_mutation_builder.rs
@@ -4,6 +4,7 @@ use core_plugin_interface::core_model::mapped_arena::{MappedArena, SerializableS
 use postgres_model::{
     access::Access,
     operation::{PostgresMutationKind, UpdateDataParameter},
+    relation::PostgresRelation,
     types::{
         PostgresCompositeType, PostgresField, PostgresFieldType, PostgresType, PostgresTypeKind,
         PostgresTypeModifier,
@@ -195,20 +196,23 @@ impl DataParamBuilder<UpdateDataParameter> for UpdateMutationBuilder {
                 .first()
                 .map(|tpe| {
                     let base_type = tpe.1.clone();
-                    let mut fields_with_id = Vec::with_capacity(base_type.fields.len() + 1);
+                    let mut base_type_fields = base_type.fields;
 
-                    let model_pk_field = model_type.pk_field().unwrap();
+                    let base_type_pk_field = base_type_fields
+                        .iter_mut()
+                        .find(|f| matches!(f.relation, PostgresRelation::Pk { .. }));
 
-                    let update_pk_field = PostgresField {
-                        typ: model_pk_field.typ.clone(),
-                        ..model_pk_field.clone()
+                    // For a non-nested type ("base type"), we already have the PK field, but it is optional. So here
+                    // we make it required (by not wrapping the model_pk_field it as optional)
+                    if let Some(base_type_pk_field) = base_type_pk_field {
+                        let model_pk_field = model_type.pk_field().unwrap();
+                        base_type_pk_field.typ = model_pk_field.typ.clone();
+                    } else {
+                        panic!("Expected a PK field in the base type")
                     };
-                    fields_with_id.push(update_pk_field);
-
-                    fields_with_id.extend(base_type.fields.into_iter());
 
                     let type_with_id = PostgresCompositeType {
-                        fields: fields_with_id,
+                        fields: base_type_fields,
                         ..base_type
                     };
 
