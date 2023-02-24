76561b3ee933b32ea4137ea75cd66cc60041c59e
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-02-23 18:23:19 -0800

    Fix React 17 incompatibility

--- graphiql/src/index.tsx
+++ graphiql/src/index.tsx
@@ -1,7 +1,6 @@
-import * as ReactDOMClient from 'react-dom/client';
+import * as ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
 const container = document.getElementById('root');
-const root = ReactDOMClient.createRoot(container as HTMLElement);
-root.render(<App/>);
+const root = ReactDOM.render(<App/>, container as HTMLElement);
