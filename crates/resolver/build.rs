a179ef7399528e288037a719e777e74e39600640
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-02-23 18:49:35 -0800

    Use Tree Sitter as a library and auto-build graphiql (#640)
    * Use Tree Sitter as a library instead of as a binary
    
    * Drop old refs to tree-sitter-cli
    
    * Build graphiql in build.rs
    
    * Fix lint
    
    * Fix npm ci failure
    
    * Fix React 17 incompatibility
    
    * Override React 18
    
    * Fix tsc failure

---
+++ crates/resolver/build.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let graphiql_folder_path = std::env::current_dir().unwrap().join("../../graphiql");
+    let graphiql_folder = graphiql_folder_path.to_str().unwrap();
+
+    println!("cargo:rerun-if-changed={graphiql_folder}/src");
+    println!("cargo:rerun-if-changed={graphiql_folder}/public");
+    println!("cargo:rerun-if-changed={graphiql_folder}/package.json");
+    println!("cargo:rerun-if-changed={graphiql_folder}/package-lock.json");
+
+    if !std::process::Command::new("npm")
+        .arg("ci")
+        .current_dir(&graphiql_folder_path)
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap()
+        .success()
+    {
+        panic!("Failed to install graphiql dependencies");
+    }
+
+    if !std::process::Command::new("npm")
+        .arg("run")
+        .arg("prod-build")
+        .current_dir(graphiql_folder_path)
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap()
+        .success()
+    {
+        panic!("Failed to build graphiql");
+    }
+}
