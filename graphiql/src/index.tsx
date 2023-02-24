730cb83f7d23e3894361337875c7f8bbca951dfd
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-02-23 18:35:26 -0800

    Override React 18

--- graphiql/src/index.tsx
+++ graphiql/src/index.tsx
@@ -1,6 +1,7 @@
-import * as ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom';
 import './index.css';
 import App from './App';
 
 const container = document.getElementById('root');
-const root = ReactDOM.render(<App/>, container as HTMLElement);
+const root = ReactDOMClient.createRoot(container as HTMLElement);
+root.render(<App/>);
