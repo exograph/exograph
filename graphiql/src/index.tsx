5b658d8efa903bf4a7fea27936ec2eb3c65ed064
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-02-23 18:40:20 -0800

    Fix tsc failure

--- graphiql/src/index.tsx
+++ graphiql/src/index.tsx
@@ -1,7 +1,7 @@
-import * as ReactDOMClient from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
 const container = document.getElementById('root');
-const root = ReactDOMClient.createRoot(container as HTMLElement);
+const root = createRoot(container as HTMLElement);
 root.render(<App/>);
