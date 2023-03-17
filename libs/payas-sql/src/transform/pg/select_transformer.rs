43d2eb0325fa786fa3da42eaa5824dbdede891e0
Ramnivas Laddad <ramnivas@payalabs.com>
2023-03-17 11:11:27 -0700

    WIP: Docs for select_transformer

--- libs/payas-sql/src/transform/pg/select_transformer.rs
+++ libs/payas-sql/src/transform/pg/select_transformer.rs
@@ -28,6 +28,83 @@ use crate::{
 use super::Postgres;
 
 impl SelectTransformer for Postgres {
+    ///
+    /// The implementation makes the assumption that the return value of the select statement is a JSON object.
+    ///
+    /// There are two axis to deal with here:
+    /// - The assembly of the return value
+    /// - The selection of the rows to assemble the return value from
+    ///
+    /// So for example, if we have a GraphQL query like:
+    /// ```graphql
+    /// {
+    ///    concerts(where: {id: {gt: 10}}, orderBy: {id: asc}, limit: 10, offset: 20) {
+    ///       id
+    ///       title
+    ///       venue {
+    ///         id
+    ///         name
+    ///      }
+    ///    }
+    /// }
+    /// ```
+    ///
+    /// We will need form:
+    /// - A json aggregate to assemble the return value. In this case, something like:
+    /// ```sql
+    /// COALESCE(
+    ///     json_agg(
+    ///         json_build_object(
+    ///             'id', "concerts"."id",
+    ///             'title', "concerts"."title",
+    ///             'venue', (SELECT json_build_object(
+    ///                 'id', "venues"."id",
+    ///                 'name', "venues"."name") FROM "venues" WHERE "concerts"."venueid" = "venues"."id")
+    ///         )
+    ///     ), '[]'::json
+    /// )::text
+    /// ```
+    ///
+    /// # Implementation notes:
+    ///
+    /// We first analyze the selection to determine characteristics of the selection:
+    /// - Does it use any order-by, limit or offset?
+    /// - Does it use any one-to-many clauses (in either predicate or order-by clause)?
+    ///
+    /// ## Single table selection without order-by, limit or offset
+    ///
+    /// The simplest case is when the selection is a single table selection with possibly a
+    /// predicate but **without** any order-by, limit, or offset. In this case, we can simply return
+    /// a direct select statement (no subselect or join). Typically, we will produce a statement
+    /// like:
+    ///
+    /// - For a single row selection:
+    /// ```sql
+    /// SELECT json_build_object('id', "concerts"."id")::text FROM "concerts" WHERE "concerts"."id" = $1
+    /// ```
+    /// - For a multiple rows selection:
+    /// ```
+    /// SELECT COALESCE(json_agg(json_build_object('id', "concerts"."id")), '[]'::json)::text FROM "concerts" WHERE "concerts"."id" > $1
+    /// ```
+    ///
+    /// ## Single table selection with order-by, limit or offset
+    ///
+    /// When the selection has an order-by, limit or offset, we need to use a subselect so that we can use
+    /// order-by, limit and offset (which don't make sense when the return value is an aggregate (like a `json_agg`)).
+    ///
+    /// Since we can provide an order-by, limit, or offset cause only to a multi-row selection, we need not worry about the single-row case.
+    ///
+    /// Here we produce a statement like:
+    ///
+    /// ```sql
+    /// SELECT COALESCE(json_agg(json_build_object('id', "concerts"."id")), '[]'::json)::text FROM (
+    ///     SELECT "concerts".* FROM "concerts" WHERE "concerts"."id" > $1 LIMIT $2 OFFSET $3
+    /// ) AS "concerts"
+    /// ```
+    /// It is the subselect's job to apply the order-by, limit and offset and return all the columns of the table.
+    /// (A possible optimization would be to only return the columns that are needed by the selection.)
+    ///
+    ///
     #[instrument(
         name = "SelectTransformer::to_select for Postgres"
         skip(self)
@@ -65,6 +142,18 @@ impl SelectTransformer for Postgres {
             .chain(order_by_column_paths.into_iter())
             .collect();
 
+        // let is_single_table_select = columns_paths.iter().any(|path| path.len() == 1);
+
+        // if is_single_table_select {
+        //     return single_table_select(
+        //         abstract_select,
+        //         additional_predicate,
+        //         selection_level,
+        //         self,
+        //         self,
+        //     );
+        // }
+
         let has_top_many_to_one_clause = columns_paths
             .iter()
             .any(|path| path[0].self_column.0.is_pk && path[0].linked_column.is_some());
@@ -76,14 +165,19 @@ impl SelectTransformer for Postgres {
 
         let join = join_util::compute_join(abstract_select.table, columns_paths);
 
-        let columns = match abstract_select.selection.to_sql(self) {
-            SelectionSQL::Single(elem) => vec![elem],
-            SelectionSQL::Seq(elems) => elems,
-        };
+        println!("Join: {:?}", crate::sql::ExpressionBuilder::into_sql(&join));
+
+        let selection_columns = abstract_select.selection.selection_columns(self);
 
         let has_non_predicate_clauses = abstract_select.order_by.is_some()
             || abstract_select.offset.is_some()
             || abstract_select.limit.is_some();
+
+        dbg!(
+            has_top_many_to_one_clause,
+            has_a_many_to_one_clause,
+            has_non_predicate_clauses
+        );
 
         if has_a_many_to_one_clause || has_non_predicate_clauses {
             let inner_select = {
@@ -165,7 +259,7 @@ impl SelectTransformer for Postgres {
                     select: Box::new(inner_select),
                     alias: Some(abstract_select.table.name.clone()),
                 },
-                columns,
+                columns: selection_columns,
                 predicate: ConcretePredicate::True,
                 order_by: None,
                 offset: None,
@@ -179,16 +273,19 @@ impl SelectTransformer for Postgres {
                 additional_predicate.unwrap_or(ConcretePredicate::True),
             );
 
-            Select {
+            let x = Select {
                 table: join,
-                columns,
+                columns: selection_columns,
                 predicate,
                 order_by: abstract_select.order_by.as_ref().map(|ob| ob.order_by()),
                 offset: abstract_select.offset.clone(),
                 limit: abstract_select.limit.clone(),
                 group_by,
                 top_level_selection: selection_level == SelectionLevel::TopLevel,
-            }
+            };
+
+            println!("Select: {:?}", crate::sql::ExpressionBuilder::into_sql(&x));
+            x
         }
     }
 
@@ -205,8 +302,36 @@ impl SelectTransformer for Postgres {
     }
 }
 
+/// Compute select for a simple query that involves only one table.
+/// This common case is optimized to avoid the joins or subselects.
+fn single_table_select<'a>(
+    abstract_select: &AbstractSelect<'a>,
+    additional_predicate: Option<ConcretePredicate<'a>>,
+    selection_level: SelectionLevel,
+    select_transformer: &impl SelectTransformer,
+    predicate_transformer: &impl PredicateTransformer,
+) -> Select<'a> {
+    let predicate: crate::Predicate<Column<'a>> = ConcretePredicate::and(
+        predicate_transformer.to_join_predicate(&abstract_select.predicate),
+        additional_predicate.unwrap_or(ConcretePredicate::True),
+    );
+
+    Select {
+        table: Table::Physical(abstract_select.table),
+        columns: abstract_select
+            .selection
+            .selection_columns(select_transformer),
+        predicate,
+        order_by: abstract_select.order_by.as_ref().map(|ob| ob.order_by()),
+        offset: abstract_select.offset.clone(),
+        limit: abstract_select.limit.clone(),
+        group_by: None,
+        top_level_selection: selection_level == SelectionLevel::TopLevel,
+    }
+}
+
 impl<'a> Selection<'a> {
-    pub fn to_sql(&self, database_kind: &impl SelectTransformer) -> SelectionSQL<'a> {
+    pub fn to_sql(&self, select_transformer: &impl SelectTransformer) -> SelectionSQL<'a> {
         match self {
             Selection::Seq(seq) => SelectionSQL::Seq(
                 seq.iter()
@@ -214,7 +339,7 @@ impl<'a> Selection<'a> {
                         |ColumnSelection {
                              alias: _alias,
                              column,
-                         }| column.to_sql(database_kind),
+                         }| column.to_sql(select_transformer),
                     )
                     .collect(),
             ),
@@ -222,7 +347,7 @@ impl<'a> Selection<'a> {
                 let object_elems = seq
                     .iter()
                     .map(|ColumnSelection { alias, column }| {
-                        JsonObjectElement::new(alias.clone(), column.to_sql(database_kind))
+                        JsonObjectElement::new(alias.clone(), column.to_sql(select_transformer))
                     })
                     .collect();
 
@@ -235,6 +360,16 @@ impl<'a> Selection<'a> {
                     }
                 }
             }
+        }
+    }
+
+    pub fn selection_columns(
+        &self,
+        select_transformer: &impl SelectTransformer,
+    ) -> Vec<Column<'a>> {
+        match self.to_sql(select_transformer) {
+            SelectionSQL::Single(elem) => vec![elem],
+            SelectionSQL::Seq(elems) => elems,
         }
     }
 }
