33c8ce58b7a0c5f6023edc769a11ec7bfe4c34f6
Ramnivas Laddad <ramnivas@payalabs.com>
2023-03-22 19:17:23 -0700

    Upgrade deno (#679)
    The major change is in how we invoke async functions (see https://github.com/denoland/deno/pull/17899), so instead of, for example `core.ops.op_claytip_execute_query_priv(query_string, variables, context_override)`, we use `core.ops.op_claytip_execute_query_priv(query_string, variables, context_override)`. Without this change we see failures with an error message: "ERROR payas_deno::deno_module > err=TypeError: invalid promise id: expected type `v8::data::Integer`, got `v8::data::Value`"
    
    Also, add a unit test for invoking Rust async operation through JavaScript to test/debug such issues in payas-deno itself.

--- crates/deno-subsystem/deno-resolver/src/operation_shim.js
+++ crates/deno-subsystem/deno-resolver/src/operation_shim.js
@@ -3,7 +3,7 @@
       return Deno[Deno.internal].core.ops.op_operation_name()
   },
   proceed: async function () {
-      return await Deno[Deno.internal].core.ops.op_operation_proceed()
+      return await Deno[Deno.internal].core.opAsync("op_operation_proceed")
   },
   query: function () {
       return Deno[Deno.internal].core.ops.op_operation_query()
