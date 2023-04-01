f806e92ae89b7b67cdc273546f79a3edd99075a4
Ramnivas Laddad <ramnivas@exograph.dev>
2023-03-31 17:30:20 -0700

    Change logo and favicon for the GraphiQL app (#693)
    Also,
    - upgrade `graphiql` and related packages.
    - make integration tests a bit less flaky.

--- graphiql/src/App.tsx
+++ graphiql/src/App.tsx
@@ -1,13 +1,44 @@
+import { useState, useEffect, useRef, useCallback } from 'react'
 import GraphiQL from "graphiql";
 import { createGraphiQLFetcher } from "@graphiql/toolkit";
+import { useTheme } from "@graphiql/react";
 
 import "graphiql/graphiql.min.css";
 
-const Logo = () => (
-  <a href="https://exograph.dev" target="_blank" rel="noreferrer">
-    <img src="logo.svg" className="logo" alt="Exograph" />
-  </a>
-);
+export const useBrowserTheme = () => {
+  const mql = useRef(window.matchMedia("(prefers-color-scheme: dark)")).current;
+
+  const currentTheme = useCallback(() => {
+    return mql.matches ? "dark" : "light";
+  }, [mql]);
+
+  const [theme, setTheme] = useState(currentTheme());
+
+  useEffect(() => {
+    const setCurrentTheme = () => {
+      setTheme(currentTheme());
+    };
+    mql.addEventListener("change", setCurrentTheme);
+    return () => mql.removeEventListener("change", setCurrentTheme);
+  }, [currentTheme, mql]);
+
+  return theme
+}
+
+function Logo() {
+  const graphiqlTheme = useTheme().theme;
+  const browserTheme = useBrowserTheme();
+
+  // Currently, switching mode in GraphiQL doesn't update the logo, but this will get fixed
+  // when https://github.com/graphql/graphiql/pull/2971 is merged.
+  const logo = graphiqlTheme === "dark" || browserTheme === "dark" ? "logo-dark.svg" : "logo-light.svg";
+
+  return (
+    <a href="https://exograph.dev" target="_blank" rel="noreferrer">
+      <img src={logo} className="logo" alt="Exograph" />
+    </a>
+  );
+};
 
 const fetcher = createGraphiQLFetcher({
   url: (window as any).exoGraphQLEndpoint,
