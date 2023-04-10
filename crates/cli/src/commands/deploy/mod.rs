2f706c8549c1f1e14455a69223d0b6038e49f31b
Ramnivas Laddad <ramnivas@exograph.dev>
2023-04-10 13:01:22 -0700

    Restructrue CLI to colocate commands and execution (#706)
    So far, structure for all commands and code to extract argument
    was defined in the main.rs file. That made is hard to find the
    correspondance between the command and the code that executes it.
    
    This commit introduces the `CommandDefinition` trait that has a method
    to return the structure and another to execute the command. This
    encapsulates both aspects into a single place.

--- crates/cli/src/commands/deploy/mod.rs
+++ crates/cli/src/commands/deploy/mod.rs
@@ -1,1 +1,11 @@
-pub(crate) mod fly;
+use super::command::SubcommandDefinition;
+
+mod fly;
+
+pub fn command_definition() -> SubcommandDefinition {
+    SubcommandDefinition::new(
+        "deploy",
+        "Deploy your Exograph project",
+        vec![Box::new(fly::FlyCommandDefinition {})],
+    )
+}
