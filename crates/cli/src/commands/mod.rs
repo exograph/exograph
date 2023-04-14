61c6ebdecca25ace0428afb64b90c8ed35fd0fe0
Ramnivas Laddad <ramnivas@exograph.dev>
2023-04-14 09:26:33 -0700

    WIP

--- crates/cli/src/commands/mod.rs
+++ crates/cli/src/commands/mod.rs
@@ -2,9 +2,11 @@
 
 pub(crate) mod build;
 pub(crate) mod command;
+pub(super) mod db;
 pub(crate) mod deploy;
 pub(crate) mod dev;
 pub(crate) mod new;
 pub(crate) mod schema;
 pub(crate) mod test;
+pub(super) mod util;
 pub(crate) mod yolo;
