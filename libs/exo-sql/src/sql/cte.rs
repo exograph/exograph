9ecda8a6659010b3d1657ff92eec6d08a703e154
Ramnivas Laddad <ramnivas@exograph.dev>
2023-04-19 21:22:50 -0700

    Add LICENSE
    * Add LICENSE file
    * Remove comments from deny.toml to keep it cleaner
    * Add third-party license file
    * Add header to most files

--- libs/exo-sql/src/sql/cte.rs
+++ libs/exo-sql/src/sql/cte.rs
@@ -1,3 +1,12 @@
+// Copyright Exograph, Inc. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file at the root of this repository.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
 use super::{select::Select, sql_operation::SQLOperation, ExpressionBuilder, SQLBuilder};
 
 /// A query with common table expressions of the form `WITH <expressions> <select>`.
--- libs/exo-sql/src/sql/mod.rs
+++ libs/exo-sql/src/sql/mod.rs
@@ -1,3 +1,12 @@
+// Copyright Exograph, Inc. All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file at the root of this repository.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
 //! Low-level SQL primitives with a few PostgreSQL-specific extensions (json_agg, json_object, etc.)
 //! We form these primitives from the higher-level SQL operations from the `asql` module. Primitives
 //! such as `Select` and `Insert` are used to execute the corresponding SQL operations on the
