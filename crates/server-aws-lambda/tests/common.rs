892cabdf4f68dc79581ee6354c8ab5a71a4b0eb3
Ramnivas Laddad <ramnivas@exograph.dev>
2023-05-26 21:57:46 -0700

    Build server-aws-lambda only on Linux platforms (#774)
    Since, we don't need to build the server-aws-lambda crate on Mac/Windows platforms, to save build
    times (for Mac platforms) and to avoid build/test issues (for Windows platforms), we build the
    server-aws-lambda crate only on Linux platforms.
    
    Ideally, we could have included the server-aws-lambda crate in the workspace only on Linux
    platforms, but until https://github.com/rust-lang/cargo/issues/5220 is fixed, we can't do that.

--- crates/server-aws-lambda/tests/common.rs
+++ crates/server-aws-lambda/tests/common.rs
@@ -7,6 +7,8 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
+#![cfg(target_os = "linux")]
+
 use std::sync::Arc;
 
 use resolver::create_system_resolver_from_serialized_bytes;
