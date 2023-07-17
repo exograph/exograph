f67620d8603861bcd1ed630f94b5ff43050388c3
Ramnivas Laddad <ramnivas@exograph.dev>
2023-07-17 13:36:08 -0700

    Allow tests to specify unordered arrays
    Often, the test writer does not care about the order of the elements in the output array. In fact,
    in the case of the Postgres module, unless `@orderBy` is specified, the order of the elements is not
    guaranteed. In such cases, we may see false failures due to a mismatch in the order.
    
    Furthermore, in our integration tests, we want to ensure that system's behavior is correct
    regardless even if `orderBy` is not specified. Especially by specifying `orderBy` we affect the
    query strategy, which may lead to different than one with no `orderBy` specified.
    
    In this PR, we allow the test writer to use the `@unordered` directive to specify that the order of
    the elements in the output array does not matter.
    
    As an example, we use this directive in one test in this PR. We will expand the usage as we go.
    
    Fixes #850

--- crates/testing/src/exotest/assertion/assert.js
+++ crates/testing/src/exotest/assertion/assert.js
@@ -19,61 +19,145 @@ export async function evaluate(testvariables) {
     return JSON.parse(JSON.stringify(json));
 }
 
-export async function test(actualPayload, testvariables) {
+export async function test(actualPayload, testvariables, unorderedSelections) {
     var $ = testvariables;
 
     // substituted in from Rust
     const expectedPayload = "%%JSON%%";
 
-    var lastKey = undefined;
+    await assert_equals(expectedPayload, actualPayload, [], unorderedSelections);
+}
 
-    async function _test(expected, actual) {
-        switch (typeof(expected)) {
-            case "object": {
+async function assert_equals(expected, actual, path, unorderedSelections) {
+    switch (typeof (expected)) {
+        case "object": {
+            if (Array.isArray(expected)) {
+                await assert_array_equal(expected, actual, path, unorderedSelections);
+            } else {
                 // recursively verify that all key/values in expectedResponse are present in actualValue
                 for (const key in expected) {
-                    lastKey = key;
                     const expectedValue = expected[key];
                     const actualValue = actual[key];
 
-                    await _test(expectedValue, actualValue, testvariables);
+                    const new_path = [...path, key];
+
+                    await assert_equals(expectedValue, actualValue, new_path, unorderedSelections);
                 }
 
                 // recursively verify that no extraneous key/values are present in actualValue
                 for (const key in actual) {
                     if (expected[key] === undefined) {
-                        throw new ExographError("unexpected key " + key.toString() + " in actual response")
+                        throw new ExographError("unexpected key " + key.toString() + " in actual response " + " at " + path_to_string(path))
                     }
                 }
-
-                break;
             }
-            case "function": {
-                let result = expected(actual);
 
-                if (result === undefined) {
-                    throw new ExographError("assertion function for field " + lastKey + " did not return a value, cannot check")
-                }
+            break;
+        }
+        case "function": {
+            let result = expected(actual);
 
-                // if this function is a Promise, resolve the promise before asserting
-                if (Object.getPrototypeOf(result) === Promise.prototype) {
-                    result = await result;
-                }
-
-                // assert true
-                if (result === false) {
-                    throw new ExographError("assert function failed for field " + lastKey + "!\nactual: " + JSON.stringify(actual))
-                }
-                break;
+            if (result === undefined) {
+                throw new ExographError("assertion function for field " + path_to_string(path) + " did not return a value, cannot check")
             }
-            default: {
-                if (expected !== actual) {
-                    throw new ExographError("assert failed: expected " + expected + " on key " + lastKey + ", got " + actual)
+
+            // if this function is a Promise, resolve the promise before asserting
+            if (Object.getPrototypeOf(result) === Promise.prototype) {
+                result = await result;
+            }
+
+            // assert true
+            if (result === false) {
+                throw new ExographError("assert function failed for field " + path_to_string(path) + "!\nactual: " + JSON.stringify(actual))
+            }
+            break;
+        }
+        default: {
+            if (expected !== actual) {
+                throw new ExographError("assert failed: expected " + expected + " on key " + path_to_string(path) + ", got " + actual)
+            }
+            break;
+        }
+    }
+}
+
+async function assert_array_equal(expected, actual, path, unorderedSelections) {
+    if (expected.length !== actual.length) {
+        throw new ExographError("assert failed: expected array length " + expected.length + ", got " + actual.length)
+    }
+    const unordered = array_contains_path(unorderedSelections, path);
+
+    if (unordered) {
+        await assert_equal_unordeded(expected, actual, path, unorderedSelections);
+    } else {
+        // We still assert one by one, since at a lower level we may have unordered arrays
+        for (let i = 0; i < expected.length; i++) {
+            const expected_item = expected[i];
+            const actual_item = actual[i];
+            await assert_equals(expected_item, actual_item, path, unorderedSelections);
+        }
+    }
+}
+
+async function assert_equal_unordeded(expected, actual, path, unorderedSelections) {
+    if (expected.length !== actual.length) {
+        throw new ExographError("assert failed: expected array length " + expected.length + ", got " + actual.length)
+    }
+
+    if (expected.length !== 0) {
+        const expected_item = expected.pop();
+
+        let match = false;
+        for (let i = 0; i < actual.length; i++) {
+            const actual_item = actual[i];
+
+            try {
+                await assert_equals(expected_item, actual_item, path, unorderedSelections);
+                // An element matched, remove it from the actual array
+                actual.splice(i, 1);
+                match = true;
+                break;
+            } catch (e) {
+                if (e instanceof ExographError) {
+                    // ignore (we might find a match later)
+                } else {
+                    throw e;
                 }
+            }
+        }
+
+        if (match) {
+            // Test the remaining items in the array
+            await assert_equal_unordeded(expected, actual, path, unorderedSelections);
+        } else {
+            throw new ExographError("assert failed: could not find a match for " + JSON.stringify(expected_item) + " at " + path_to_string(path)) + " in the actual array";
+        }
+    }
+}
+
+function array_contains_path(paths, path) {
+    for (const item of paths) {
+        if (item.length !== path.length) {
+            continue;
+        }
+
+        let match = true;
+        for (let i = 0; i < item.length; i++) {
+            if (item[i] !== path[i]) {
+                match = false;
                 break;
             }
         }
+
+        if (match) {
+            return true;
+        }
     }
 
-    await _test(expectedPayload, actualPayload);
+    return false;
+}
+
+// Nicer path for printing (e.g. 'a.b.c')
+function path_to_string(path) {
+    return "'" + path.join(".") + "'";
 }
