5d7e41a705fd3a9baaf3adb7c987ea43c6ef0c25
Luke Taylor <tekul@pm.me>
2023-07-31 23:21:18 +0100

    Update to work with Deno 1.34.1
    Builds against the patched_1_34_1 fork of the exograph fork which
    is slightly ahead of 1.34.1 on Deno's main branch.

--- crates/core-subsystem/core-resolver/src/plugin/subsystem_resolver.rs
+++ crates/core-subsystem/core-resolver/src/plugin/subsystem_resolver.rs
@@ -37,13 +37,13 @@ pub trait SubsystemResolver: Sync {
     /// Shim method for `resolve`
     async fn resolve_cdylib<'a>(
         &'a self,
-        handle: Handle,
+        _handle: Handle,
         operation: &'a ValidatedField,
         operation_type: OperationType,
         request_context: &'a RequestContext,
         system_resolver: &'a SystemResolver,
     ) -> Result<Option<QueryResponse>, SubsystemResolutionError> {
-        let _guard = handle.enter();
+        // let _guard = handle.enter();
         self.resolve(operation, operation_type, request_context, system_resolver)
             .await
     }
@@ -51,13 +51,13 @@ pub trait SubsystemResolver: Sync {
     /// Shim method for `invoke_interceptor`
     async fn invoke_interceptor_cdylib<'a>(
         &'a self,
-        handle: Handle,
+        _handle: Handle,
         interceptor_index: InterceptorIndex,
         intercepted_operation: &'a InterceptedOperation,
         request_context: &'a RequestContext<'a>,
         system_resolver: &'a SystemResolver,
     ) -> Result<Option<QueryResponse>, SubsystemResolutionError> {
-        let _guard = handle.enter();
+        // let _guard = handle.enter();
         self.invoke_interceptor(
             interceptor_index,
             intercepted_operation,
