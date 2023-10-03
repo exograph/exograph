e7037a447679b74ebcaed99f3fccab550d1156c8
Ramnivas Laddad <ramnivas@exograph.dev>
2023-10-03 09:59:32 -0700

    Clear up the disk space in CI

---
+++ .github/workflows/clear-disk-space.sh
@@ -0,0 +1,6 @@
+sudo rm -rf /usr/share/dotnet
+sudo rm -rf /opt/ghc
+sudo rm -rf /usr/local/graalvm
+sudo rm -rf /usr/local/lib/android
+sudo rm -rf /usr/local/share/boost
+sudo rm -rf $AGENT_TOOLSDIRECTORY
