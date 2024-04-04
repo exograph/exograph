d0ea38937115cbbb0d0f8f9990984b10ffd417dc
Ramnivas Laddad <ramnivas@exograph.dev>
2024-04-04 15:02:22 -0700

    Revert "Try Windows-2019 to see if performance is better"
    This reverts commit de0a337f2b552a84a8cadfee51d1cbf46b77ee8a.

--- .github/workflows/install-pgvector.bat
+++ .github/workflows/install-pgvector.bat
@@ -3,6 +3,6 @@ cd $RUNNER_TEMP
 git clone --branch v0.6.2 https://github.com/pgvector/pgvector.git
 cd pgvector
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 nmake /NOLOGO /F Makefile.win
 nmake /NOLOGO /F Makefile.win install
