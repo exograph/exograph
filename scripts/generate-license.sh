e8dd02e27d7da05d50c02a7f5281161824375bfc
Ramnivas Laddad <ramnivas@exograph.dev>
2024-07-08 17:18:11 -0700

    [skip ci] Update the third party licenses file

--- scripts/generate-license.sh
+++ scripts/generate-license.sh
@@ -2,4 +2,14 @@
 
 SCRIPT_DIR=`dirname -- "$0"`;
 
-cargo about generate $SCRIPT_DIR/about.hbs > $SCRIPT_DIR/../licenses.html
+cargo about generate $SCRIPT_DIR/about.hbs | npx @html-to/text-cli --preserveNewlines > $SCRIPT_DIR/../THIRD-PARTY-LICENSES
+npx generate-license-file --config $SCRIPT_DIR/generate-license-file-config.json --overwrite
+
+sed -i 's/“/"/g' $SCRIPT_DIR/../THIRD-PARTY-LICENSES
+sed -i 's/”/"/g' $SCRIPT_DIR/../THIRD-PARTY-LICENSES
+sed -i 's/&lt;/</g' $SCRIPT_DIR/../THIRD-PARTY-LICENSES
+sed -i 's/&gt;/>/g' $SCRIPT_DIR/../THIRD-PARTY-LICENSES
+
+cat $SCRIPT_DIR/npm-licenses.txt >> $SCRIPT_DIR/../THIRD-PARTY-LICENSES
+
+rm $SCRIPT_DIR/npm-licenses.txt
