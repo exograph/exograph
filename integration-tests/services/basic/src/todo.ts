e7ca75dd2a950efd23bdcde3a183853c57df43de
Sebastian <sebas5384@gmail.com>
2024-07-15 22:44:06 -0300

    Fix deno's code generation for collection return type (#1166)
    Fixes #1041

--- integration-tests/services/basic/src/todo.ts
+++ integration-tests/services/basic/src/todo.ts
@@ -1,6 +1,15 @@
-import type { Todo } from "../generated/TodoModule.d.ts";
+import type { Todo, Todos } from "../generated/TodoModule.d.ts";
 
 export async function todo(id: number): Promise<Todo> {
   const r = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
   return r.json();
+}
+
+export async function todos(): Promise<Todos> {
+  const r = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+  const todos = await r.json();
+  return {
+    totalCount: todos.length,
+    items: todos,
+  };
 }
