db7f1f7ea2d92f808ee5e96e4f05cf0ebe2de92c
Ramnivas Laddad <ramnivas@exograph.dev>
2024-10-22 13:59:00 -0700

    Upgrade Deno to 2.0.2

--- libs/exo-deno/src/deno_module.rs
+++ libs/exo-deno/src/deno_module.rs
@@ -26,11 +26,13 @@ use deno_runtime::deno_io::Stdio;
 use deno_runtime::deno_node::DenoFsNodeResolverEnv;
 use deno_runtime::deno_node::NodeExtInitServices;
 use deno_runtime::deno_node::NodeRequireResolver;
-use deno_runtime::deno_node::NpmProcessStateProvider;
 use deno_runtime::deno_permissions::PermissionsContainer;
 use deno_runtime::deno_web::BlobStore;
+use deno_runtime::ops::process::NpmProcessStateProvider;
+use deno_runtime::permissions::RuntimePermissionDescriptorParser;
 use deno_runtime::worker::MainWorker;
 use deno_runtime::worker::WorkerOptions;
+use deno_runtime::worker::WorkerServiceOptions;
 use deno_runtime::BootstrapOptions;
 use deno_runtime::WorkerExecutionMode;
 use deno_semver::package::PackageNv;
@@ -45,6 +47,7 @@ use node_resolver::errors::{PackageFolderResolveError, PackageNotFoundError};
 use node_resolver::{NodeResolver, NpmResolver};
 use tracing::error;
 
+use std::borrow::Cow;
 use std::cell::RefCell;
 use std::path::PathBuf;
 use tracing::instrument;
@@ -188,20 +191,23 @@ impl NpmResolver for SnapshotNpmResolver {
 }
 
 impl NodeRequireResolver for SnapshotNpmResolver {
-    fn ensure_read_permission(
+    fn ensure_read_permission<'a>(
         &self,
         _permissions: &mut dyn deno_runtime::deno_node::NodePermissions,
-        path: &std::path::Path,
-    ) -> Result<(), AnyError> {
+        path: &'a std::path::Path,
+    ) -> Result<Cow<'a, std::path::Path>, AnyError> {
         if path.starts_with(&self.registry_base) {
-            Ok(())
+            Ok(Cow::Borrowed(path))
         } else {
             bail!("Expected path to be in the registry")
         }
     }
 }
 
-impl NpmProcessStateProvider for SnapshotNpmResolver {
+#[derive(Debug)]
+struct NpmProcessStateProviderImpl;
+
+impl NpmProcessStateProvider for NpmProcessStateProviderImpl {
     fn get_npm_process_state(&self) -> String {
         String::new()
     }
@@ -374,7 +380,6 @@ impl DenoModule {
                 location: None,
                 no_color: false,
                 color_level: deno_terminal::colors::get_color_level(),
-                unstable: true,
                 mode: WorkerExecutionMode::None,
                 is_stdout_tty: false,
                 is_stderr_tty: false,
@@ -385,10 +390,7 @@ impl DenoModule {
                 enable_op_summary_metrics: false,
                 unstable_features: vec![],
                 node_ipc_fd: None,
-                disable_deprecated_api_warning: false,
-                verbose_deprecated_api_warning: false,
                 argv0: None,
-                future: false,
                 serve_host: None,
                 serve_port: None,
                 node_debug: None,
@@ -396,43 +398,47 @@ impl DenoModule {
             create_params: None,
             extensions,
             unsafely_ignore_certificate_errors: None,
-            root_cert_store_provider: None,
             seed: None,
             create_web_worker_cb,
             maybe_inspector_server: None,
             should_break_on_first_statement: false,
-            module_loader,
             get_error_class_fn: Some(&get_error_class_name),
             origin_storage_dir: None,
-            blob_store: shared_state.blob_store,
-            broadcast_channel: shared_state.broadcast_channel,
-            shared_array_buffer_store: None,
-            compiled_wasm_module_store: None,
             format_js_error_fn: None,
-            fs,
             stdio: Stdio::default(),
+            cache_storage_dir: None,
+            should_wait_for_inspector_session: false,
+            startup_snapshot: Some(crate::deno_snapshot()),
+            skip_op_registration: false,
+            strace_ops: None,
+        };
+
+        let main_module = deno_core::resolve_url(&main_module_specifier)?;
+        let permission_desc_parser = Arc::new(RuntimePermissionDescriptorParser::new(fs.clone()));
+        let permissions = PermissionsContainer::allow_all(permission_desc_parser);
+
+        let services = WorkerServiceOptions {
+            root_cert_store_provider: None,
+            module_loader,
+            fs,
             node_services: (npm_resolver.zip(node_resolver)).map(
                 |(npm_resolver, node_resolver)| NodeExtInitServices {
                     node_require_resolver: npm_resolver.clone(),
                     node_resolver: node_resolver.clone(),
-                    npm_process_state_provider: npm_resolver.clone(),
                     npm_resolver: npm_resolver.clone(),
                 },
             ),
-            cache_storage_dir: None,
-            should_wait_for_inspector_session: false,
-            startup_snapshot: Some(crate::deno_snapshot()),
+            npm_process_state_provider: Some(Arc::new(NpmProcessStateProviderImpl)),
+            blob_store: shared_state.blob_store,
+            broadcast_channel: shared_state.broadcast_channel.clone(),
+            shared_array_buffer_store: None,
+            compiled_wasm_module_store: None,
             feature_checker: Default::default(),
-            skip_op_registration: false,
-            strace_ops: None,
+            permissions,
             v8_code_cache: None,
         };
 
-        let main_module = deno_core::resolve_url(&main_module_specifier)?;
-        let permissions = PermissionsContainer::allow_all();
-
-        let mut worker =
-            MainWorker::bootstrap_from_options(main_module.clone(), permissions, options);
+        let mut worker = MainWorker::bootstrap_from_options(main_module.clone(), services, options);
 
         worker.execute_main_module(&main_module).await?;
 
