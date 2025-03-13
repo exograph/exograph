18497cfae9114d90308c7f864f0c576badeac5f7
Ramnivas Laddad <ramnivas@exograph.dev>
2025-03-13 12:04:04 -0700

    Do not include lsp when building on wasm targets

--- crates/lsp/src/main.rs
+++ crates/lsp/src/main.rs
@@ -7,6 +7,8 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
+#![cfg(not(target_family = "wasm"))]
+
 use anyhow::Result;
 use server::Backend;
 use tower_lsp::{LspService, Server};
