620ef5cb7be8cc1848ee94c55794c9ea2b413b45
Ramnivas Laddad <ramnivas@exograph.dev>
2025-06-24 18:36:20 -0700

    Address more review comments

--- .github/actions/setup-pgvector-windows/install-pgvector.bat
+++ .github/actions/setup-pgvector-windows/install-pgvector.bat
@@ -1,4 +1,10 @@
-set "PGROOT=C:\Program Files\PostgreSQL\14"
+REM Accept parameters with defaults
+set "PG_VERSION=%1"
+set "PGVECTOR_VERSION=%2"
+if "%PG_VERSION%"=="" set "PG_VERSION=14"
+if "%PGVECTOR_VERSION%"=="" set "PGVECTOR_VERSION=0.6.2"
+
+set "PGROOT=C:\Program Files\PostgreSQL\%PG_VERSION%"
 
 REM Check if pgvector is already installed (cache hit)
 if exist "%PGROOT%\lib\pgvector.dll" (
@@ -8,7 +14,7 @@ if exist "%PGROOT%\lib\pgvector.dll" (
 
 echo pgvector not found, compiling from source...
 cd %RUNNER_TEMP%
-git clone --branch v0.6.2 https://github.com/pgvector/pgvector.git
+git clone --branch v%PGVECTOR_VERSION% https://github.com/pgvector/pgvector.git
 cd pgvector
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
