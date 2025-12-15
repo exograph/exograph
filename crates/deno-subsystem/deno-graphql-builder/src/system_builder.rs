879402c46809ad916274bd579377779244984295
Ramnivas Laddad <ramnivas@exograph.dev>
2025-12-15 15:29:16 -0800

    chore: upgrade to Deno 2.6.1

--- crates/deno-subsystem/deno-graphql-builder/src/system_builder.rs
+++ crates/deno-subsystem/deno-graphql-builder/src/system_builder.rs
@@ -36,7 +36,7 @@ use url::Url;
 
 use crate::module_skeleton_generator;
 
-const DENO_VERSION: &str = "2.6.0";
+const DENO_VERSION: &str = "2.6.1";
 
 const DENO_BUNDLE_WARNING: &[u8] = b"is experimental and subject to changes";
 
